b4abd676e36709fb1b93e4ac690e3acbfebd8e9d
Cle C <clemak27@mailbox.org>
2019-06-08 16:06:52 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,19 +10,19 @@
 sudo pacman -Syyu
 sudo pacman -R manjaro-browser-settings ms-office-online
 
-sudo pacman -S yay youtube-dl mpv gimp nextcloud-client keepassxc cmatrix lolcat neofetch ripgrep fzf
+sudo pacman -S yay youtube-dl mpv gimp keepassxc cmatrix lolcat neofetch ripgrep fzf syncthing-gtk
 
-sudo pacman -S libreoffice-fresh libreoffice-fresh-de texlive-most
+sudo pacman -S git make gcc docker docker-compose jdk8-openjdk maven vim neovim nodejs npm yarn
+yay -S vscodium-bin skypeforlinux-stable-bin
 
-sudo pacman -S git make gcc docker docker-compose jdk8-openjdk maven vim neovim
-yay -S vscodium-bin tty-clock
-
+# git config
 git config --global user.name "clemak27"
 git config --global user.email clemak27@mailbox.org
 git config --global alias.lol 'log --graph --decorate --oneline --all'
 git config --global core.autocrlf input
 git config --global credential.helper "cache --timeout=18000"
 
+# vs code
 code --install-extension akamud.vscode-theme-onedark
 code --install-extension dakara.transformer
 code --install-extension eamodio.gitlens
@@ -32,20 +32,19 @@ code --install-extension visualstudioexptteam.vscodeintellicode
 code --install-extension james-yu.latex-workshop
 
 echo "" >> ~/.bashrc
-echo "use aliases from file" >> ~/.bashrc
+echo "# use aliases from file" >> ~/.bashrc
 echo "if [ -f ~/.bash_aliases ]; then" >> ~/.bashrc
 echo "        . ~/.bash_aliases" >> ~/.bashrc
 echo "fi" >> ~/.bashrc
 echo "" >> ~/.bashrc
-echo "use ripgrep with fzf" >> ~/.bashrc
+echo "# use ripgrep with fzf" >> ~/.bashrc
 echo "export FZF_DEFAULT_COMMAND='rg --files --hidden'" >> ~/.bashrc
 
 cp vimrc ~/.vimrc
 cp bash_aliases ~/.bash_aliases
 
+# gaming
 sudo pacman -S wine-staging lutris
 
-# nextcloud symlinken wenn ordner runtergeladen
-# rm ~/Documents/ -R && ln -s ~/Nextcloud/Documents/ ~/Documents
-# rm ~/Pictures/ -R && ln -s ~/Nextcloud/Pictures/ ~/Pictures
-
+# office
+sudo pacman -S libreoffice-fresh libreoffice-fresh-de texlive-most
