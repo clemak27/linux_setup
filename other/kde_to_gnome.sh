c57eba624fc78d7211782b1f14f6b4595d829e1c
clemak27 <clemak27@mailbox.org>
2019-07-27 16:37:19 +0200

    some more modifications

--- other/kde_to_gnome.sh
+++ other/kde_to_gnome.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # remove plasma and kde applications
-sudo pacman -Rs --noconfirm bluedevil breeze breeze-gtk kactivitymanagerd kde-cli-tools kde-gtk-config kdecoration kdeplasma-addons kgamma5 khotkeys kinfocenter kmenuedit knetattach kscreen kscreenlocker ksshaskpass ksysguard kwallet-pam kwayland-integration kwin kwrited libkscreen libksysguard milou plasma-browser-integration plasma-desktop plasma-integration plasma-nm plasma-pa plasma-workspace plasma-workspace-wallpapers polkit-kde-agent powerdevil sddm-kcm systemsettings user-manager
-sudo pacman -Rs --noconfirm ark dolphin dolphin-plugins ffmpegthumbs filelight gwenview kaccounts-integration kaccounts-providers kamera kate kcalc kdegraphics-thumbnailers kdenetwork-filesharing kdialog keditbookmarks kfind kget khelpcenter kio-extras konsole ksystemlog kwalletmanager okular print-manager signon-kwallet-extension spectacle kdeconnect
+sudo pacman -Rs --noconfirm bluedevil breeze breeze-gtk kactivitymanagerd kde-cli-tools kde-gtk-config kdecoration kdeplasma-addons kgamma5 khotkeys kinfocenter kmenuedit knetattach kscreen kscreenlocker ksshaskpass ksysguard kwallet-pam kwayland-integration kwin kwrited libkscreen libksysguard milou plasma-browser-integration plasma-desktop plasma-integration plasma-nm plasma-pa plasma-workspace plasma-workspace-wallpapers polkit-kde-agent powerdevil sddm-kcm systemsettings user-manager kget
+sudo pacman -Rs --noconfirm ark dolphin dolphin-plugins ffmpegthumbs filelight gwenview kaccounts-integration kaccounts-providers kamera kate kcalc kdegraphics-thumbnailers kdenetwork-filesharing kdialog keditbookmarks kfind khelpcenter kio-extras konsole ksystemlog kwalletmanager okular print-manager signon-kwallet-extension spectacle kdeconnect
 sudo pacman -Rs --noconfirm latte-dock mpd cantata kid3 redshift plasma-browser-integration kvantum-qt5 seahorse
 sudo pacman -Rs --noconfirm breeze
-sudo pacman -Rs --noconfirm cmake extra-cmake-modules kwindowsystem kdecoration kcoreaddons
+sudo pacman -Rs --noconfirm cmake extra-cmake-modules kwindowsystem kdecoration kcoreaddons partitionmanager
 
 sudo systemctl disable sddm
 
 # gnome
-sudo pacman -S --noconfirm baobab eog epiphany evince file-roller gdm gedit gnome-backgrounds gnome-books gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-contacts gnome-control-center gnome-dictionary gnome-disk-utility gnome-font-viewer gnome-keyring gnome-logs gnome-maps gnome-menus gnome-photos gnome-remote-desktop gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-themes-extra gnome-todo gnome-user-docs gnome-user-share gnome-video-effects gnome-weather grilo-plugins gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb mousetweaks mutter nautilus networkmanager orca rygel sushi tracker tracker-miners vino xdg-user-dirs-gtk yelp gnome-software simple-scan
+sudo pacman -S --noconfirm baobab eog epiphany evince file-roller gdm gedit gnome-backgrounds gnome-books gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-contacts gnome-control-center gnome-dictionary gnome-disk-utility gnome-font-viewer gnome-keyring gnome-logs gnome-menus gnome-photos gnome-remote-desktop gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-themes-extra gnome-todo gnome-user-docs gnome-user-share gnome-video-effects gnome-weather grilo-plugins gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb mousetweaks mutter nautilus networkmanager orca rygel sushi tracker tracker-miners vino xdg-user-dirs-gtk yelp simple-scan
 
 # pther gnome
 sudo pacman -S --noconfirm lollypop kvantum-qt5 tilix gnome-tweaks syncthing-gtk curl
@@ -21,8 +21,8 @@ sudo systemctl enable gdm
 sudo pacman -S --noconfirm networkmanager
 sudo systemctl enable NetworkManager
 
+cd ..
 # ONLY WORKS WHEN RUNNING GNOME
-# cd ..
 # chmod a+x ./gnome/gnome-shell-extension-installer.sh
 # ./gnome/gnome-shell-extension-installer.sh 15 # alternatetab
 # ./gnome/gnome-shell-extension-installer.sh 615 # appindicator-support
@@ -38,4 +38,3 @@ cp gnome/one-dark.json ~/.config/tilix/schemes/
 rm -R ~/.themes/*
 # install vimix-kde and gtk
 # eventually move these to the appropiate places
-sudo pacman -Rs systemsettings gnome-books partitionmanager systemsettings gnome-software ksysguard gnome-maps syncthingtray epiphany thunderbird plasma-desktop kde-gtk-config
