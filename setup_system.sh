1dbd2c0148f3a5d4ccf69018316522d31b53fe92
clemak27 <clemak27@mailbox.org>
2019-08-15 18:12:28 +0200

    add pim applications and partitionmanager

--- setup_system.sh
+++ setup_system.sh
@@ -49,10 +49,13 @@ pacman -Syyu
 pacman -S --noconfirm bluedevil breeze breeze-gtk kactivitymanagerd kde-cli-tools kde-gtk-config kdecoration kdeplasma-addons kgamma5 khotkeys kinfocenter kmenuedit knetattach kscreen kscreenlocker ksshaskpass ksysguard kwallet-pam kwayland-integration kwin kwrited libkscreen libksysguard milou plasma-browser-integration plasma-desktop plasma-integration plasma-nm plasma-pa plasma-workspace plasma-workspace-wallpapers polkit-kde-agent powerdevil sddm-kcm systemsettings user-manager
 
 # kde-applications
-pacman -S --noconfirm ark dolphin dolphin-plugins ffmpegthumbs filelight gwenview kaccounts-integration kaccounts-providers kamera kate kcalc kdegraphics-thumbnailers kdenetwork-filesharing kdialog keditbookmarks kfind kget khelpcenter kio-extras konsole ksystemlog kwalletmanager okular print-manager signon-kwallet-extension spectacle kdeconnect
+pacman -S --noconfirm ark dolphin dolphin-plugins ffmpegthumbs filelight gwenview kaccounts-integration kaccounts-providers kamera kate kcalc kdegraphics-thumbnailers kdenetwork-filesharing kdialog keditbookmarks kfind kget khelpcenter kio-extras konsole ksystemlog kwalletmanager okular print-manager signon-kwallet-extension spectacle kdeconnect partitionmanager
 
 # kde-specifics
 pacman -S --noconfirm latte-dock mpd cantata kid3 redshift plasma-browser-integration seahorse
+
+# kde pim
+pacman -S --noconfirm kmail kaddressbook korganizer kdepim-addon
 
 systemctl enable sddm
 
