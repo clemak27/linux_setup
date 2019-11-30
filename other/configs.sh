92da99f392263a95b9fed9f9d5be71b78c238100
clemak27 <clemak27@mailbox.org>
2019-11-30 22:01:22 +0100

    add sweet vim colorscheme

--- other/configs.sh
+++ other/configs.sh
@@ -3,6 +3,8 @@
 # export config
 cp ../dotfiles/zshrc /home/cle/.zshrc
 cp ../dotfiles/vimrc /home/cle/.config/nvim/init.vim
+mkdir -p /home/cle/.config/nvim/colors
+cp ../dotfiles/ksweet.vim /home/cle/.config/nvim/colors/ksweet.vim
 
 # TODO dont hardcode this xD
-cp ../ff/chrome/* /home/cle/.mozilla/firefox/zfn1qc4b.default-release/chrome/
+# cp ../ff/chrome/* /home/cle/.mozilla/firefox/zfn1qc4b.default-release/chrome/
