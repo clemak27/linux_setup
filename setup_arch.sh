b02ac82fe14610a10fe55ad4d7d3d9dceb2fd1e3
clemak27 <clemak27@mailbox.org>
2020-03-06 21:07:05 +0100

    setup boot for encrypted device

--- setup_arch.sh
+++ setup_arch.sh
@@ -49,7 +49,10 @@ arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bo
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 # setup boot for encrypted device
-
+sed -i 's/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev autodetect keyboard keymap modconf block encrypt filesystems fsck)/g' /etc/mkinitcpio.conf
+arch-chroot /mnt mkinitcpio -p linux
+sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cryptdevice="'${device}'2":luks:allow-discards"/g' /etc/default/grub
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 # setup_system
 cp setup_system.sh /mnt/setup_system.sh
