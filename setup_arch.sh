999004b56dcd9661d36c28c539554e3f48b453f6
clemak27 <clemak27@mailbox.org>
2020-03-06 20:14:32 +0100

    fix efi path

--- setup_arch.sh
+++ setup_arch.sh
@@ -16,19 +16,20 @@ loadkeys de-latin1
 timedatectl set-ntp true
 
 # Setup the disk and partitions
-
 parted --script "${device}" -- mklabel gpt \
   mkpart primary fat32 1MiB 261MiB \
   set 1 esp on \
   mkpart primary ext4 261MiB 100%
 
+# encrypt root
 echo -n "${passphrase}" | cryptsetup -v luksFormat "${device}2" -
 echo -n "${passphrase}" | cryptsetup open "${device}2" cryptroot -
 
+# create filesystems
 mkfs.fat -F32 "${device}1"
 mkfs.ext4 /dev/mapper/cryptroot
 
-mkdir -p /mnt/boot/efi
+# mount partitions
 mount "${device}1" /mnt/boot/
 mount /dev/mapper/cryptroot /mnt/
 
@@ -42,9 +43,13 @@ pacstrap /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # install bootloader
+mkdir -p /mnt/boot/efi
 arch-chroot /mnt pacman -S --noconfirm grub efibootmgr intel-ucode
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+# setup boot for encrypted device
+
 
 # setup_system
 cp setup_system.sh /mnt/setup_system.sh
