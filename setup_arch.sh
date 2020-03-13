974ff4f8a9ab03207173f45129a3506559f75d72
clemak27 <clemak27@mailbox.org>
2020-03-13 20:57:36 +0100

    add check if config exists

--- setup_arch.sh
+++ setup_arch.sh
@@ -6,6 +6,9 @@ trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 # Load config
 if [ -f ./config.sh ]; then
     source ./config.sh
+else
+   echo "Config file could not be found!"
+   exit 1
 fi
 
 # Set the keyboard layout
