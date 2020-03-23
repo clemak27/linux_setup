257d5b68cf55c69cb6639ee121b2ddd89660ed68
clemak27 <clemak27@mailbox.org>
2020-03-23 22:15:00 +0100

    add test for copying

---
+++ tests/copy.test.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import os.path
+
+commands = []
+fails = []
+
+def scanFile(filepath):
+    for line in open(filepath):
+        if 'cp ' in line:
+            packagesInLine = line.replace('cp ', '').replace('\n','')
+            commandPaths = packagesInLine.split(' ')
+            commands.append(commandPaths[0])
+
+print("Checking if all files that should be copied exist.")
+
+scanFile('./setup_arch')
+scanFile('./setup_system')
+scanFile('./setup_user')
+
+for filePath in commands:
+    if not os.path.exists(filePath):
+        fails.append(filePath)
+
+if len(fails) > 0:
+    print("Test failed:", len(fails) , "files not found:")
+    print(fails)
+    raise AssertionError()
+else:
+    print("Test successful, all files to copy found.")
