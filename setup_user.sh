763264f80e2bf341bc18f67c5a523228c589c2f6
clemak27 <clemak27@mailbox.org>
2020-04-12 17:32:32 +0200

    make user setup depend on modules

--- setup_user.sh
+++ setup_user.sh
@@ -3,6 +3,14 @@
 set -uo pipefail
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
+# Load config
+if [ -f ./config.sh ]; then
+  source ./config.sh
+else
+  echo "Config file could not be found!"
+  exit 1
+fi
+
 #------
 
 # https://zren.github.io/kde/
@@ -24,31 +32,35 @@ cp dotfiles/zshrc ~/.zshrc
 chsh -s /usr/bin/zsh
 
 # python dev packages
-pip install jedi pylint --user
+if [[ " ${modules[@]} " =~ "python" ]]; then
+  pip install jedi pylint --user
+fi
 
 # go dev binaries
-mkdir -p ~/.go
-export GOPATH=~/.go
-go get github.com/klauspost/asmfmt/cmd/asmfmt
-go get github.com/go-delve/delve/cmd/dlv
-go get github.com/kisielk/errcheck
-go get github.com/davidrjenni/reftools/cmd/fillstruct
-go get github.com/mdempsky/gocode
-go get github.com/stamblerre/gocode
-go get github.com/rogpeppe/godef
-go get github.com/zmb3/gogetdoc
-go get golang.org/x/tools/cmd/goimports
-go get golang.org/x/lint/golint
-go get golang.org/x/tools/gopls
-go get github.com/golangci/golangci-lint/cmd/golangci-lint
-go get github.com/fatih/gomodifytags
-go get golang.org/x/tools/cmd/gorename
-go get github.com/jstemmer/gotags
-go get golang.org/x/tools/cmd/guru
-go get github.com/josharian/impl
-go get honnef.co/go/tools/cmd/keyify
-go get github.com/fatih/motion
-go get github.com/koron/iferr
+if [[ " ${modules[@]} " =~ "go" ]]; then
+  mkdir -p ~/.go
+  export GOPATH=~/.go
+  go get github.com/klauspost/asmfmt/cmd/asmfmt
+  go get github.com/go-delve/delve/cmd/dlv
+  go get github.com/kisielk/errcheck
+  go get github.com/davidrjenni/reftools/cmd/fillstruct
+  go get github.com/mdempsky/gocode
+  go get github.com/stamblerre/gocode
+  go get github.com/rogpeppe/godef
+  go get github.com/zmb3/gogetdoc
+  go get golang.org/x/tools/cmd/goimports
+  go get golang.org/x/lint/golint
+  go get golang.org/x/tools/gopls
+  go get github.com/golangci/golangci-lint/cmd/golangci-lint
+  go get github.com/fatih/gomodifytags
+  go get golang.org/x/tools/cmd/gorename
+  go get github.com/jstemmer/gotags
+  go get golang.org/x/tools/cmd/guru
+  go get github.com/josharian/impl
+  go get honnef.co/go/tools/cmd/keyify
+  go get github.com/fatih/motion
+  go get github.com/koron/iferr
+fi
 
 # rust dev
 # path schon in zshrc
@@ -124,8 +136,8 @@ cp ./kde/BreezeBlackCustom.colors ~/.local/share/color-schemes
 
 # intellij: material theme
 # kde theme:
-    # colorscheme for konsole one dark in folder
-    # colors -> brezze black custom
-    # plasma theme breeze
-    # window decorations breeze
-    # icons candy icons
+# colorscheme for konsole one dark in folder
+# colors -> brezze black custom
+# plasma theme breeze
+# window decorations breeze
+# icons candy icons
