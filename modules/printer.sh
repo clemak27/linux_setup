7173a105e5bc50d0978d8961281647fae49bb2a7
clemak27 <clemak27@mailbox.org>
2020-06-03 23:10:58 +0200

    extend printer setup

--- modules/printer.sh
+++ modules/printer.sh
@@ -4,5 +4,12 @@ set -uo pipefail
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
 # printer
-pacman -S --noconfirm cups
+pacman -S --noconfirm cups system-config-printer
 systemctl enable org.cups.cupsd.service
+
+#------user------
+
+cat <<EOT >> setup_user.sh
+yay -S brother-dcpj572dw
+
+EOT
