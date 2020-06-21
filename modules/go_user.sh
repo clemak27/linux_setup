4234b3598bb595aeb65754e687c1cee334255338
clemak27 <clemak27@mailbox.org>
2020-06-21 16:57:19 +0200

    fix go setup

---
+++ modules/go_user.sh
@@ -0,0 +1,25 @@
+
+echo "Installing go packages"
+mkdir -p ~/.go
+export GOPATH=~/.go
+go get -v github.com/klauspost/asmfmt/cmd/asmfmt
+go get -v github.com/go-delve/delve/cmd/dlv
+go get -v github.com/kisielk/errcheck
+go get -v github.com/davidrjenni/reftools/cmd/fillstruct
+go get -v github.com/mdempsky/gocode
+go get -v github.com/stamblerre/gocode
+go get -v github.com/rogpeppe/godef
+go get -v github.com/zmb3/gogetdoc
+go get -v golang.org/x/tools/cmd/goimports
+go get -v golang.org/x/lint/golint
+go get -v golang.org/x/tools/gopls
+go get -v github.com/golangci/golangci-lint/cmd/golangci-lint
+go get -v github.com/fatih/gomodifytags
+go get -v golang.org/x/tools/cmd/gorename
+go get -v github.com/jstemmer/gotags
+go get -v golang.org/x/tools/cmd/guru
+go get -v github.com/josharian/impl
+go get -v honnef.co/go/tools/cmd/keyify
+go get -v github.com/fatih/motion
+go get -v github.com/koron/iferr
+
