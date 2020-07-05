feef41a87ca55a9558a86b19b0f869eaac67a4f1
clemak27 <clemak27@mailbox.org>
2020-07-05 19:50:53 +0200

    add obs studio

--- modules/gaming.sh
+++ modules/gaming.sh
@@ -4,7 +4,7 @@ set -uo pipefail
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
 # gaming
-pacman -S --noconfirm wine-staging lutris steam discord
+pacman -S --noconfirm wine-staging lutris steam discord obs-studio
 
 #------user------
 
