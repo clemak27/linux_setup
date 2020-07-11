6025d0f406ef3d126bd08a85f9b592f914825c71
clemak27 <clemak27@mailbox.org>
2020-07-11 12:04:10 +0200

    symlink firefox customization

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -4,4 +4,7 @@ ln -sf ~/Projects/linux_setup/dotfiles/vimrc ~/.config/nvim/init.vim
 ln -sf ~/Projects/linux_setup/dotfiles/zshrc ~/.zshrc
 ln -sf ~/Projects/linux_setup/dotfiles/p10k ~/.p10k.zsh
 
+ffProfilePath="/home/cle/.mozilla/firefox/72zvuvdy.default-release"
 
+ln -sf ~/Projects/linux_setup/ff/chrome/userChrome.css $ffProfilePath/chrome/userChrome.css
+ln -sf ~/Projects/linux_setup/ff/chrome/userContent.css $ffProfilePath/chrome/userContent.css
