71247c40f2d14df14e9c656d06304bc11411bb56
clemak27 <clemak27@mailbox.org>
2020-07-11 11:43:26 +0200

    create script for symlinking stuff

---
+++ scripts/symlinks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -sf ~/Projects/linux_setup/dotfiles/vimrc ~/.config/nvim/init.vim
+ln -sf ~/Projects/linux_setup/dotfiles/zshrc ~/.zshrc
+ln -sf ~/Projects/linux_setup/dotfiles/p10k ~/.p10k.zsh
+
+
