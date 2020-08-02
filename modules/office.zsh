697589e98917185784f4ad5867d10c5a31bc34ad
clemak27 <clemak27@mailbox.org>
2020-08-02 15:09:54 +0200

    moved printer setup to office

--- modules/office.zsh
+++ modules/office.zsh
@@ -4,3 +4,20 @@
 
 # office
 pacman -S --noconfirm libreoffice-fresh libreoffice-fresh-de texlive-most
+
+# printer
+pacman -S --noconfirm cups system-config-printer
+systemctl enable org.cups.cupsd.service
+
+# user-setup
+declare -a user_commands
+user_commands=(
+  'yay -S brother-dcpj572dw'
+)
+declare -r user_commands
+IFS=$SAVEIFS
+
+for task in "${user_commands[@]}"
+do
+  echo "$task" >> setup_user.zsh
+done
