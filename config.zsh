b06127b5dcb953aaab3f8a7c8010476b1d45fbac
clemak27 <clemak27@mailbox.org>
2020-08-02 15:52:47 +0200

    add config checks

--- config.zsh
+++ config.zsh
@@ -12,7 +12,6 @@ gpu="nvidia"
 
 declare -a system_modules
 system_modules=(
-asdasdasd
 #  plasma
 #  gpu
 #  gui
@@ -48,3 +47,58 @@ do
      exit 1
   fi
 done
+
+if [ -z "$device" ]; then
+  echo "device not set!"
+  exit 1
+fi
+
+if [ -z "$passphrase" ]; then
+  echo "passphrase not set!"
+  exit 1
+fi
+
+if [ -z "$luksMapper" ]; then
+  echo "luksMapper not set!"
+  exit 1
+fi
+
+if [ -z "$volumeGroup" ]; then
+  echo "volumeGroup not set!"
+  exit 1
+fi
+
+if [ -z "$hostname" ]; then
+  echo "hostname not set!"
+  exit 1
+fi
+
+if [ -z "$user" ]; then
+  echo "user not set!"
+  exit 1
+fi
+
+if [ -z "$password" ]; then
+  echo "password not set!"
+  exit 1
+fi
+
+if [ -z "$cpu" ]; then
+  echo "cpu not set!"
+  exit 1
+fi
+
+if [ "$cpu" != "intel" ] | [ "$cpu" != "amd" ]; then
+  echo "cpu set to unknown value!"
+  exit 1
+fi
+
+if [ -z "$gpu" ]; then
+  echo "gpu not set!"
+  exit 1
+fi
+
+if [ "$gpu" != "nvidia" ] | [ "$gpu" != "amd" ]; then
+  echo "gpu set to unknown value!"
+  exit 1
+fi
