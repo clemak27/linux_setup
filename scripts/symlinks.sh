d503a1141e7b3595e92c0291b5b851707f0c3743
clemak27 <clemak27@mailbox.org>
2020-08-06 22:53:25 +0200

    add oc-config.json

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ln -sf ~/Projects/linux_setup/dotfiles/vimrc ~/.config/nvim/init.vim
+ln -sf ~/Projects/linux_setup/dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
 ln -sf ~/Projects/linux_setup/dotfiles/zshrc ~/.zshrc
 ln -sf ~/Projects/linux_setup/dotfiles/starship.toml ~/.starship.toml
 
