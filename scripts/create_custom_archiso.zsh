6aca9505c13122450a8b33b3a312fc01e657b7a5
clemak27 <clemak27@mailbox.org>
2020-08-28 15:52:59 +0200

    fix path

--- scripts/create_custom_archiso.zsh
+++ scripts/create_custom_archiso.zsh
@@ -4,7 +4,6 @@ sudo pacman -S archiso
 
 sudo cp -r /usr/share/archiso/configs/releng/ archiso
 cd archiso
-ls
-sudo /bin/bash ./build.sh -v -w /tmp/archiso-tmp -o /home/cle/Desktop
+sudo /bin/bash ./build.sh -v -w /tmp/archiso-tmp -o ~/Desktop
 
 sudo pacman -Rns archiso
