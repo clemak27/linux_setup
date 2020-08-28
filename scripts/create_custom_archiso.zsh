97859874640bc55387f3437b0348e8d6d32b708d
clemak27 <clemak27@mailbox.org>
2020-08-28 15:53:08 +0200

    add noconfirm

--- scripts/create_custom_archiso.zsh
+++ scripts/create_custom_archiso.zsh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
-sudo pacman -S archiso
+sudo pacman -S --noconfirm archiso
 
 sudo cp -r /usr/share/archiso/configs/releng/ archiso
 cd archiso
 sudo /bin/bash ./build.sh -v -w /tmp/archiso-tmp -o ~/Desktop
 
-sudo pacman -Rns archiso
+sudo pacman -Rns --noconfirm archiso
