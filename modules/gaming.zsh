b264b2edb7ce3da06375fc7ff1a13655193bd8d3
clemak27 <clemak27@mailbox.org>
2020-09-12 12:16:40 +0200

    add gaming notes

--- modules/gaming.zsh
+++ modules/gaming.zsh
@@ -11,9 +11,6 @@ SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 user_commands=(
   'yay -S --noconfirm steam-fonts'
-  ''
-  '# kill all gta V processes'
-  '# killall -9 -r ".*\.exe|.*SocialClub.*|.*Rockstar.*" && kill -9 $(ps aux | grep '.*PlayGTA.*' | awk '{print $2}')'
 )
 declare -r user_commands
 IFS=$SAVEIFS
@@ -22,3 +19,12 @@ for task in "${user_commands[@]}"
 do
   echo "$task" >> setup_user.zsh
 done
+
+# ------------------------ Notes ------------------------
+
+# kill all gta V processes
+# killall -9 -r ".*\.exe|.*SocialClub.*|.*Rockstar.*" && kill -9 $(ps aux | grep '.*PlayGTA.*' | awk '{print $2}')
+#
+# I've fixed this issue by changing the pointing launcher link of the game.
+# Actually in steam the game is launched through the gameguide launcher that causes some troubles. Indeed, the gameguide launcher works but the play button in it won't launch the game.
+# So to start it directly from the game launcher instead of the gameguide launcher, go to steam/common/Sid Meier's Civilization VI/ and edit "Civ6" file and change the line "./GameGuide/Civ6" to "./Civ6Sub"
