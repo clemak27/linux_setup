25e0a6b2b5b28df6fd0f2ad088c16c8622ced401
clemak27 <clemak27@mailbox.org>
2020-09-14 23:19:46 +0200

    update archiso script

--- scripts/create_custom_archiso.zsh
+++ scripts/create_custom_archiso.zsh
@@ -1,15 +1,16 @@
 #!/bin/zsh
 
-setupPath="~/Projects/linux_setup/"
-outPath="~/Desktop"
+setupPath="/home/cle/Projects/linux_setup/"
+outPath="/home/cle/Desktop"
 
 sudo pacman -S --noconfirm archiso
 
 sudo cp -r /usr/share/archiso/configs/releng/ /tmp/archiso
 
 sudo cp -r $setupPath /tmp/archiso/airootfs/linux_setup
+echo 'android-tools' | sudo tee -a /tmp/archiso/packages.x86_64
 
-sudo /bin/bash /tmp/archiso/build.sh -v -w /tmp/archiso-tmp -o $outPath
+sudo mkarchiso -v -w /tmp/archiso-tmp -o $outPath /tmp/archiso
 
 sudo rm -rf /tmp/archiso /tmp/archiso-tmp
 
