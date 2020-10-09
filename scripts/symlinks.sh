69b70e606c9c6e697c635026504b94a8ae121c3e
clemak27 <clemak27@mailbox.org>
2020-10-09 16:16:10 +0200

    symlink stuff

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-ln -sf ~/Projects/linux_setup/dotfiles/vimrc ~/.config/nvim/init.vim
-ln -sf ~/Projects/linux_setup/dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
+# zsh
 ln -sf ~/Projects/linux_setup/dotfiles/zshrc ~/.zshrc
 ln -sf ~/Projects/linux_setup/dotfiles/starship.toml ~/.starship.toml
+
+# nvim
+ln -sf ~/Projects/linux_setup/dotfiles/vimrc ~/.config/nvim/init.vim
+ln -sf ~/Projects/linux_setup/dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
+
+# intelliJ
 ln -sf ~/Projects/linux_setup/dotfiles/ideavimrc ~/.ideavimrc
+
+# ranger
 ln -sf ~/Projects/linux_setup/dotfiles/ranger.rc ~/.config/ranger/rc.conf
 ln -sf ~/Projects/linux_setup/dotfiles/ranger.commands ~/.config/ranger/commands.py
 
+# rofi
+ln -sf ~/Projects/linux_setup/rofi/clean.rasi ~/.config/rofi/themes/clean.rasi
+ln -sf ~/Projects/linux_setup/rofi/colors.rasi ~/.config/rofi/themes/colors.rasi
+ln -sf ~/Projects/linux_setup/rofi/onedark.rasi ~/.config/rofi/themes/onedark.rasi
+
+# spicetify
 ln -sf ~/Projects/linux_setup/dotfiles/spicetify ~/.config/spicetify/config.ini
 
+# firefox
 ffProfilePath="~/.mozilla/firefox/72zvuvdy.default-release"
-
 ln -sf ~/Projects/linux_setup/ff/chrome/userChrome.css $ffProfilePath/chrome/userChrome.css
 ln -sf ~/Projects/linux_setup/ff/chrome/userContent.css $ffProfilePath/chrome/userContent.css
