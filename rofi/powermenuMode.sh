6dc082de8a01fc6f387ce654d4574b55a6f78db8
clemak27 <clemak27@mailbox.org>
2020-10-09 15:20:32 +0200

    make script do stuff

--- rofi/powermenuMode.sh
+++ rofi/powermenuMode.sh
@@ -1,18 +1,22 @@
 #!/bin/zsh
 
 if [ -z "$@" ]; then
-    echo -en "Shutdown\0icon\x1fsystem-shutdown\n"
-    echo -en "Logout\0icon\x1fsystem-log-out\n"
-    echo -en "Suspend\0icon\x1fsystem-suspend\n"
-    echo -en "Reboot\0icon\x1fsystem-restart\n"
+    echo -en "Shutdown\n"
+    echo -en "Suspend\n"
+    echo -en "Reboot\n"
+    echo -en "Cancel\n"
 else
     if [ "$1" = "Shutdown" ]; then
         echo -en "Now\n30s\n1m"
-    elif [ "$1" = "Exit" ]; then
-        i3-msg exit
     elif [ "$1" = "Reboot" ]; then
         shutdown -r 0
     elif [ "$1" = "Suspend" ]; then
         systemctl suspend
+    elif [ "$1" = "Now" ]; then
+      shutdown 0
+    elif [ "$1" = "30s" ]; then
+      shutdown 30
+    elif [ "$1" = "1m" ]; then
+      shutdown 60
     fi
 fi
