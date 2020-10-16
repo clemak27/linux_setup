21f4ec3f005645347d8dc22cdcf40889d3724bcf
clemak27 <clemak27@mailbox.org>
2020-10-16 16:55:27 +0200

    add powermenu to combo launcher

--- rofi/combo.sh
+++ rofi/combo.sh
@@ -1,3 +1,8 @@
 #!/bin/zsh
 
-rofi -combi-modi window,drun -show combi -modi combi -me-select-entry '' -me-accept-entry 'MousePrimary' -theme custom
+rofi -combi-modi window,drun \
+     -show combi \
+     -modi combi,'Powermenu:~/Projects/linux_setup/rofi/powermenuMode.sh' \
+     -me-select-entry '' \
+     -me-accept-entry 'MousePrimary' \
+     -theme custom
