e76796f44b014fee4215cda5ba77519cec86397b
clemak27 <clemak27@mailbox.org>
2020-10-16 17:35:21 +0200

    rename powermenu to just power

--- rofi/rofi.zsh
+++ rofi/rofi.zsh
@@ -2,7 +2,7 @@
 
 rofi -combi-modi window,drun \
      -show combi \
-     -modi combi,'Powermenu:~/Projects/linux_setup/rofi/powermenuMode.sh' \
+     -modi combi,'power:~/Projects/linux_setup/rofi/power.zsh' \
      -me-select-entry '' \
      -me-accept-entry 'MousePrimary' \
      -theme custom
