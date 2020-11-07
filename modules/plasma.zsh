8cc37c82827f5e610722f8ea125481a7c451ac05
clemak27 <clemak27@mailbox.org>
2020-11-07 18:38:31 +0100

    migrate plasma

--- modules/plasma.zsh
+++ modules/plasma.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-# ------------------------ KDE Plasma ------------------------
+# ------------------------ pacman ------------------------
 
 # plasma
 pacman -S --noconfirm bluedevil breeze breeze-gtk kactivitymanagerd kde-cli-tools kde-gtk-config kdecoration kdeplasma-addons kgamma5 khotkeys kinfocenter kmenuedit knetattach kscreen kscreenlocker ksshaskpass ksysguard kwallet-pam kwayland-integration kwin kwrited libkscreen libksysguard milou plasma-browser-integration plasma-desktop plasma-integration plasma-nm plasma-pa plasma-workspace plasma-workspace-wallpapers polkit-kde-agent powerdevil sddm-kcm systemsettings
@@ -18,48 +18,87 @@ pacman -S --noconfirm kontact kaddressbook korganizer kmail kmail-account-wizard
 # latte-dock
 pacman -S --noconfirm latte-dock
 
-# user-setup
+# ------------------------ AUR ------------------------
+
+# setup
+
+cd /
+mkdir /home/aurBuilder
+chgrp nobody /home/aurBuilder
+chmod g+ws /home/aurBuilder
+setfacl -m u::rwx,g::rwx /home/aurBuilder
+setfacl -d --set u::rwx,g::rwx,o::- /home/aurBuilder
+usermod -d /home/aurBuilder nobody
+echo '%nobody ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+cd /home/aurBuilder
+
+# install packages
+
+declare -a aur_packages
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+aur_packages=(
+  'plasma5-applets-eventcalendar'
+  'plasma5-applets-window-appmenu'
+  'plasma5-applets-window-buttons'
+  'plasma5-applets-window-title'
+)
+
+declare -r aur_packages
+IFS=$SAVEIFS
+
+for package in "${aur_packages[@]}"
+do
+  git clone https://aur.archlinux.org/$package.git
+  chmod -R g+w $package
+  cd $package
+  sudo -u nobody makepkg -sri --noconfirm
+  cd ..
+done
+
+# cleanup
+
+sed -i '$d' /etc/sudoers
+cd /
+usermod -d / nobody
+rm -rf /home/aurBuilder
+
+# ------------------------ user ------------------------
+
 declare -a user_commands
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 user_commands=(
-  'kde widgets'
-  'yay -S plasma5-applets-eventcalendar'
-  'yay -S plasma5-applets-window-appmenu plasma5-applets-window-buttons plasma5-applets-window-title'
-  ''
-  '# meta key latte menu'
-  'kwriteconfig5 --file ~/.config/kwinrc --group ModifierOnlyShortcuts --key Meta "org.kde.lattedock,/Latte,org.kde.LatteDock,activateLauncherMenu"'
-  'qdbus org.kde.KWin /KWin reconfigure'
-  ''
-  '# Hide titlebars when maximized (useful for topbar-layout)'
-  'kwriteconfig5 --file ~/.config/kwinrc --group Windows --key BorderlessMaximizedWindows true'
-  'qdbus org.kde.KWin /KWin reconfigure'
-  ''
-  '# screen locking change picture'
-  '# logout: confirm, end current session, start with manually saved'
-  '# usermanager change picture'
-  '# regional format us region, everything else österreich'
-  '# power management anpassen'
-  '# autostart: latte, syncthing, keepass'
-  ''
-  '# 144Hz'
-  '# Add MaxFPS=144 to your ~/.config/kwinrc under [Compositing]'
-  '# Add xrandr --rate 144 to /usr/share/sddm/scripts/Xsetup'
-  '# about:config layout.frame_rate 144'
-  ''
-  '# intellij: material theme'
-  '# kde theme:'
-  '# colors -> breeze black custom'
-  '# plasma theme breeze'
-  '# window decorations breeze'
-  ''
-  '# add .../rofi/combo.sh as custom shortcut'
-  '# use alt+space'
+  'kwriteconfig5 --file ~/.config/kwinrc --group Windows --key BorderlessMaximizedWindows true' # Hide titlebars when maximized (useful for topbar-layout)
 )
 declare -r user_commands
 IFS=$SAVEIFS
 
 for task in "${user_commands[@]}"
 do
-  echo "$task" >> setup_user.zsh
+  su - $user -c $task
 done
+
+# ------------------------ notes ------------------------
+
+# screen locking change picture
+# logout: confirm, end current session, start with manually saved
+# usermanager change picture
+# regional format us region, everything else österreich
+# power management anpassen
+# autostart: latte, syncthing, keepass
+
+# 144Hz
+# Add MaxFPS=144 to your ~/.config/kwinrc under [Compositing]
+# Add xrandr --rate 144 to /usr/share/sddm/scripts/Xsetup
+# about:config layout.frame_rate 144
+
+# intellij: material theme
+# kde theme:
+# colors -> breeze black custom
+# plasma theme breeze
+# window decorations breeze
+
+# add .../rofi/combo.sh as custom shortcut
+# use alt+space
