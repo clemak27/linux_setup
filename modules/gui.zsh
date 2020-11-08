23dcb9af7e88ba989bd5e402642373f2da08ca80
clemak27 <clemak27@mailbox.org>
2020-11-08 11:59:57 +0100

    migrate gui

--- modules/gui.zsh
+++ modules/gui.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-# ------------------------ GUI programs ------------------------
+# ------------------------ pacman ------------------------
 
 # fonts
 pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra
@@ -16,35 +16,82 @@ pacman -S --noconfirm obs-studio kdenlive
 
 # rofi
 pacman -S --noconfirm rofi rofi-calc dmenu
-mkdir -p /home/$user/.config/rofi/themes
+
+# ------------------------ AUR ------------------------
+
+# setup
+
+cd /
+mkdir /home/aurBuilder
+chgrp nobody /home/aurBuilder
+chmod g+ws /home/aurBuilder
+setfacl -m u::rwx,g::rwx /home/aurBuilder
+setfacl -d --set u::rwx,g::rwx,o::- /home/aurBuilder
+usermod -d /home/aurBuilder nobody
+echo '%nobody ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+cd /home/aurBuilder
+
+# install packages
+
+declare -a aur_packages
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+aur_packages=(
+  'scrcpy'
+  'todotxt'
+  'spotify'
+  'syncthing'
+  'syncthingtray'
+)
+
+declare -r aur_packages
+IFS=$SAVEIFS
+
+for package in "${aur_packages[@]}"
+do
+  git clone https://aur.archlinux.org/$package.git
+  chmod -R g+w $package
+  cd $package
+  sudo -u nobody makepkg -sri --noconfirm
+  cd ..
+done
+
+# cleanup
+
+sed -i '$d' /etc/sudoers
+cd /linux_setup
+usermod -d / nobody
+rm -rf /home/aurBuilder
+
+# additional steps
+
+chmod a+wr /opt/spotify
+chmod a+wr /opt/spotify/Apps -R
+
+# ------------------------ user ------------------------
 
 # user-setup
 declare -a user_commands
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 user_commands=(
-  'yay -S --noconfirm scrcpy'
-  ''
-  'yay -S todotxt'
   'mkdir ~/.todo'
   'mkdir ~/.todo.actions.d'
   'cd ~/.todo.actions.d'
   'git clone https://github.com/rebeccamorgan/due.git'
   'chmod +x due/due'
-  ''
-  'yay -S --noconfirm spotify'
-  'sudo chmod a+wr /opt/spotify'
-  'sudo chmod a+wr /opt/spotify/Apps -R'
-  ''
-  'yay -S --noconfirm syncthing'
-  'yay -S --noconfirm syncthingtray'
-  'systemctl --user enable syncthing.service'
-  'systemctl --user start syncthing.service'
+  'mkdir -p /home/$user/.config/rofi/themes'
+#  'systemctl --user enable syncthing.service'
+#  'systemctl --user start syncthing.service'
 )
 declare -r user_commands
 IFS=$SAVEIFS
 
 for task in "${user_commands[@]}"
 do
-  echo "$task" >> setup_user.zsh
+  su - $user -c $task
 done
+
+# ------------------------ notes ------------------------
+
