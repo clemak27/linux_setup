23429a39b5305c9c71c1d46d929c161a45103c05
clemak27 <clemak27@mailbox.org>
2020-11-20 16:31:06 +0100

    remove rofi dotfiles

--- rofi/rofi.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-rofi -combi-modi window,drun \
-     -show combi \
-     -modi combi,calc,'power:~/Projects/linux_setup/rofi/power.zsh' \
-     -me-select-entry '' \
-     -me-accept-entry 'MousePrimary' \
-     -theme custom
