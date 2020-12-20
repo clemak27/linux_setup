e4bbf17f9bc350accb333fea1009fc127ab7ae7f
clemak27 <clemak27@mailbox.org>
2020-12-20 10:53:05 +0100

    create module file

---
+++ modules/i3.zsh
@@ -0,0 +1,60 @@
+#!/bin/zsh
+
+# ------------------------ i3 ------------------------
+
+# ------------------------ Load config ------------------------
+echo "Loading config"
+if [ -f ./config.zsh ]; then
+    source ./config.zsh
+else
+   echo "Config file could not be found!"
+   exit 1
+fi
+
+# ------------------------ pacman ------------------------
+
+# office
+pacman -S --quiet --noprogressbar --noconfirm <packages>
+
+# ------------------------ AUR ------------------------
+
+declare -a aur_packages
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+aur_packages=(
+  'pandoc-bin'
+)
+
+declare -r aur_packages
+IFS=$SAVEIFS
+
+for package in "${aur_packages[@]}"
+do
+  cd /home/aurBuilder
+  git clone https://aur.archlinux.org/$package.git
+  chmod -R g+w $package
+  cd $package
+  sudo -u nobody makepkg -sri --noconfirm
+  cd /linux_setup
+done
+
+# ------------------------ user ------------------------
+
+declare -a user_commands
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+user_commands=(
+  # kde
+  'mkdir -p ~/.local/share/konsole'
+)
+declare -r user_commands
+IFS=$SAVEIFS
+
+for task in "${user_commands[@]}"
+do
+  su - $user -c $task
+done
+
+# ------------------------ notes ------------------------
+
