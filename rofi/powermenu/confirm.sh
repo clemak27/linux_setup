4b59a892d74cca747677c00bd42e0d0cda75cb26
clemak27 <clemak27@mailbox.org>
2020-12-21 15:09:40 +0100

    style custom confirm screen

--- rofi/powermenu/confirm.sh
+++ rofi/powermenu/confirm.sh
@@ -7,7 +7,7 @@ option1="no"
 # options passed into variable
 options="$option0\n$option1"
 
-chosen="$(echo -e "$options" | rofi -lines 2 -dmenu -p "Are you sure?" -selected-row 1)"
+chosen="$(echo -e "$options" | rofi -lines 2 -dmenu -p "Are you sure?" -selected-row 1 -theme ./confirm.rasi)"
 case $chosen in
     $option0)
         echo "yes";;
