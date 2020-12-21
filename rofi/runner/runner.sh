936fce8b25af489c83540daaf4c707b5c9385be4
clemak27 <clemak27@mailbox.org>
2020-12-21 14:20:44 +0100

    move rofi runner to different folder

--- rofi/runner/runner.sh
+++ rofi/runner/runner.sh
@@ -2,7 +2,7 @@
 
 rofi -combi-modi window,drun \
      -show combi \
-     -modi combi,calc,'power:~/Projects/linux_setup/rofi/power.zsh' \
+     -modi combi,calc,'power:~/Projects/linux_setup/rofi/runner/power.sh' \
      -me-select-entry '' \
      -me-accept-entry 'MousePrimary' \
      -theme ./custom.rasi
