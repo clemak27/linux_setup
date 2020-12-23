a40f42d3234b36c520969473fe0fd0928b44d98a
clemak27 <clemak27@mailbox.org>
2020-12-23 12:34:02 +0100

    update config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,7 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar eDP-1 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar topbar 2>&1 | tee -a /tmp/polybar1.log & disown
 
 # displays=$(xrandr -q | grep " connected" | cut -d ' ' -f1)
 # exec echo $displays | grep -q "eDP1" &&
