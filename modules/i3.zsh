4a67f98665c8f9096a0f9bbf1b301412bf85566c
clemak27 <clemak27@mailbox.org>
2020-12-25 15:15:09 +0100

    remove unneeded packages

--- modules/i3.zsh
+++ modules/i3.zsh
@@ -14,33 +14,17 @@ fi
 # ------------------------ pacman ------------------------
 
 # i3 gaps
-pacman -S --quiet --noprogressbar --noconfirm i3-gaps i3blocks i3lock i3status i3status
+pacman -S --quiet --noprogressbar --noconfirm i3-gaps
 mkdir -p /home/$user/.config/i3
 ln -sf /home/$user/Projects/linux_setup/i3/config /home/$user/.config/i3/config
-
-# polybar
-ln -sf /home/$user/Projects/linux_setup/polybar/config /home/$user/.config/polybar/config
 
 # compositor
 pacman -S --quiet --noprogressbar --noconfirm picom
 mkdir -p /home/$user/.config/picom
 ln -sf /home/$user/Projects/linux_setup/picom/picom.conf /home/$user/.config/picom/picom.conf
 
-pacman -Rns --quiet --noprogressbar --noconfirm dmenu
-
 # feh for wallpaper
 pacman -S --quiet --noprogressbar --noconfirm feh
-
-# xss-lock
-pacman -S --quiet --noprogressbar --noconfirm xss-lock
-
-# notfications
-pacman -S --quiet --noprogressbar --noconfirm dunst
-mkdir -p /home/$user/.config/dunst
-ln -sf /home/$user/Projects/linux_setup/dunst/dunstrc /home/$user/.config/dunst/dunstrc
-
-# networking
-pacman -S --quiet --noprogressbar --noconfirm networkmanager nm-connection-editor bmon network-manager-applet gnome-keyring libsecret seahorse
 
 # ------------------------ AUR ------------------------
 
@@ -49,8 +33,6 @@ SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 
 aur_packages=(
-  'rofi-dmenu'
-  'polybar'
 )
 
 declare -r aur_packages
