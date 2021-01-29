b0df37c560f474d28413fbd7cb547414a48b1206
clemak27 <clemak27@mailbox.org>
2021-01-29 16:20:17 +0100

    update confirm promt

--- rofi/confirm/confirm.sh
+++ rofi/confirm/confirm.sh
@@ -3,13 +3,13 @@
 rofi_path=$HOME/Projects/linux_setup/rofi
 
 # options to be displayed
-option0="yes"
-option1="no"
+option0="     Confirm"
+option1="🗙     Cancel"
 
 # options passed into variable
 options="$option0\n$option1"
 
-chosen="$(echo -e "$options" | rofi -lines 2 -dmenu -p "Are you sure?" -selected-row 1 -theme $rofi_path/confirm/confirm.rasi)"
+chosen="$(echo -e "$options" | rofi -lines 2 -dmenu -selected-row 1 -theme $rofi_path/confirm/confirm.rasi)"
 case $chosen in
     $option0)
         echo "yes";;
