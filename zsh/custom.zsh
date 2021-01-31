b3350460436b6db54870f2dfd52414686b1a0793
clemak27 <clemak27@mailbox.org>
2021-01-31 12:17:47 +0100

    improve function

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -1,10 +1,27 @@
 #!/bin/zsh
 
 function glab_issue_description {
+  if [ "$#" -ne 1 ]; then
+    echo "missing issue number!"
+    return 1
+  fi
+
+  issue_number="$1"
+
+  glab issue view $issue_number &> /dev/null
+  if [ $? -ne 0 ]; then
+    echo "gitlab issue not found!"
+    return 1
+  fi
+
   uuid=$(uuidgen)
-  glab issue view 73 > $uuid.md
-  tail $uuid.md --lines=+8 > $uuid.md
+  glab issue view $issue_number > $uuid.md
+  desc=$(tail $uuid.md --lines=+8)
+  echo $desc > $uuid.md
   nvim $uuid.md
-  glab issue update 73 --description "$(cat -p $uuid.md)"
+  glab issue update $issue_number --description "$(bat -p $uuid.md)"
+
   rm $uuid.md
+  unset uuid
+  unset desc
 }
