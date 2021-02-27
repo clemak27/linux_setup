d5754778529b611e431ce6b9eee2b9b3e6e874e5
clemak27 <clemak27@mailbox.org>
2021-02-27 17:09:48 +0100

    move config to separate folder

--- scripts/symlinks.zsh
+++ scripts/symlinks.zsh
@@ -6,78 +6,78 @@ else
   user="$1"
 fi
 
-dotfile_dir="/home/$user/Projects/linux_setup"
+dotfile_dir="/home/$user/Projects/linux_setup/config"
 user_dir="/home/$user"
-
-# i3
-mkdir -p $user_dir/.config/i3
-ln -sf $dotfile_dir/i3/config $user_dir/.config/i3/config
-
-# picom
-mkdir -p $user_dir/.config/picom
-ln -sf $dotfile_dir/picom/picom.conf $user_dir/.config/picom/picom.conf
 
 # Xresources
 ln -sf $dotfile_dir/Xresources $user_dir/.Xresources
 
 # zsh
-ln -sf $dotfile_dir/zsh/zshrc $user_dir/.zshrc
-ln -sf $dotfile_dir/zsh/glab.zsh $user_dir/.glab.zsh
-ln -sf $dotfile_dir/zsh/curl.zsh $user_dir/.curl.zsh
-ln -sf $dotfile_dir/zsh/starship.toml $user_dir/.starship.toml
+ln -sf $dotfile_dir/zshrc $user_dir/.zshrc
+ln -sf $dotfile_dir/glab.zsh $user_dir/.glab.zsh
+ln -sf $dotfile_dir/curl.zsh $user_dir/.curl.zsh
+ln -sf $dotfile_dir/starship.toml $user_dir/.starship.toml
+
+# delta
+ln -sf $dotfile_dir/delta.config $user_dir/.delta.config
 
 # bat
 mkdir -p $user_dir/.config/bat
 ln -sf $dotfile_dir/bat/config $user_dir/.config/bat/config
-
-# nvim
-mkdir -p $user_dir/.config/nvim
-ln -sf $dotfile_dir/nvim/init.vim $user_dir/.config/nvim/init.vim
-ln -sf $dotfile_dir/nvim/coc-settings.json $user_dir/.config/nvim/coc-settings.json
-
-# kitty
-mkdir -p $user_dir/.config/kitty
-ln -sf $dotfile_dir/kitty/kitty.conf $user_dir/.config/kitty/kitty.conf
 
 # glab
 mkdir -p $user_dir/.config/glab-cli
 ln -sf $dotfile_dir/glab-cli/aliases.yml $user_dir/.config/glab-cli/aliases.yml
 ln -sf $dotfile_dir/glab-cli/config.yml $user_dir/.config/glab-cli/config.yml
 
-# delta
-ln -sf $dotfile_dir/delta/delta.config $user_dir/.delta.config
+# i3
+mkdir -p $user_dir/.config/i3
+ln -sf $dotfile_dir/i3/config $user_dir/.config/i3/config
 
 # intelliJ
-ln -sf $dotfile_dir/intelliJ/ideavimrc $user_dir/.ideavimrc
+ln -sf $dotfile_dir/ideavimrc $user_dir/.ideavimrc
+
+# kitty
+mkdir -p $user_dir/.config/kitty
+ln -sf $dotfile_dir/kitty/kitty.conf $user_dir/.config/kitty/kitty.conf
+
+# mpv
+mkdir -p $user_dir/.config/mpv
+ln -sf $dotfile_dir/mpv/mpv.conf $user_dir/.config/mpv/mpv.conf
+
+# nvim
+mkdir -p $user_dir/.config/nvim
+ln -sf $dotfile_dir/nvim/init.vim $user_dir/.config/nvim/init.vim
+ln -sf $dotfile_dir/nvim/coc-settings.json $user_dir/.config/nvim/coc-settings.json
+
+# picom
+mkdir -p $user_dir/.config/picom
+ln -sf $dotfile_dir/picom/picom.conf $user_dir/.config/picom/picom.conf
 
 # ranger
 mkdir -p $user_dir/.config/ranger
 ln -sf $dotfile_dir/ranger/ranger.rc $user_dir/.config/ranger/rc.conf
 ln -sf $dotfile_dir/ranger/ranger.commands $user_dir/.config/ranger/commands.py
 
-# todo.txt
-mkdir -p $user_dir/.todo
-ln -sf $dotfile_dir/todo.sh/todo.cfg $user_dir/.todo/config
-
-# mpv
-mkdir -p $user_dir/.config/mpv
-ln -sf $dotfile_dir/mpv/mpv.conf $user_dir/.config/mpv/mpv.conf
-
-# plasma color scheme
-mkdir -p $user_dir/.local/share/color-schemes
-ln -sf $dotfile_dir/plasma/Kustom.colors $user_dir/.local/share/color-schemes/Kustom.colors
-
 # redshift
 mkdir -p $user_dir/.config/redshift
 ln -sf $dotfile_dir/redshift/redshift.conf $user_dir/.config/redshift/redshift.conf
+
+# todo.txt
+mkdir -p $user_dir/.todo
+ln -sf $dotfile_dir/todo/todo.cfg $user_dir/.todo/config
+
+# spotify-tui
+mkdir -p $user_dir/.config/spotify-tui
+ln -sf $dotfile_dir/spotify-tui/config.yml $user_dir/.config/spotify-tui/config.yml
 
 # spotifyd
 mkdir -p $user_dir/.config/spotifyd
 ln -sf $dotfile_dir/spotifyd/spotifyd.conf $user_dir/.config/spotifyd/spotifyd.conf
 
-# spotify-tui
-mkdir -p $user_dir/.config/spotify-tui
-ln -sf $dotfile_dir/spotify-tui/config.yml $user_dir/.config/spotify-tui/config.yml
+# plasma color scheme
+mkdir -p $user_dir/.local/share/color-schemes
+ln -sf $dotfile_dir/plasma/Kustom.colors $user_dir/.local/share/color-schemes/Kustom.colors
 
 # firefox
 #ffProfilePath="$user_dir/.mozilla/firefox/72zvuvdy.default-release"
