e51d9a31e084c9be5812f845732dbaa5fc848750
clemak27 <clemak27@mailbox.org>
2021-02-27 17:33:18 +0100

    update script

--- scripts/symlinks.zsh
+++ scripts/symlinks.zsh
@@ -6,80 +6,90 @@ else
   user="$1"
 fi
 
+# ------------------------ paths ------------------------
+
 setup_dir="/home/$user/Projects/linux_setup"
-dotfile_dir="$setup_dir/config"
+config_dir="$setup_dir/config"
 user_dir="/home/$user"
 ffProfilePath=$(find $user_dir/.mozilla/firefox -name "*.default-release")
 
+# ------------------------ ~ ------------------------
+
 # Xresources
-ln -sf $dotfile_dir/Xresources $user_dir/.Xresources
+ln -sf $config_dir/Xresources $user_dir/.Xresources
 
 # zsh
-ln -sf $dotfile_dir/zshrc $user_dir/.zshrc
-ln -sf $dotfile_dir/glab.zsh $user_dir/.glab.zsh
-ln -sf $dotfile_dir/curl.zsh $user_dir/.curl.zsh
-ln -sf $dotfile_dir/starship.toml $user_dir/.starship.toml
+ln -sf $config_dir/zshrc $user_dir/.zshrc
+ln -sf $config_dir/glab.zsh $user_dir/.glab.zsh
+ln -sf $config_dir/curl.zsh $user_dir/.curl.zsh
+ln -sf $config_dir/starship.toml $user_dir/.starship.toml
 
 # delta
-ln -sf $dotfile_dir/delta.config $user_dir/.delta.config
+ln -sf $config_dir/delta.config $user_dir/.delta.config
+
+# ------------------------ .config ------------------------
 
 # bat
 mkdir -p $user_dir/.config/bat
-ln -sf $dotfile_dir/bat/config $user_dir/.config/bat/config
+ln -sf $config_dir/bat/config $user_dir/.config/bat/config
 
 # glab
 mkdir -p $user_dir/.config/glab-cli
-ln -sf $dotfile_dir/glab-cli/aliases.yml $user_dir/.config/glab-cli/aliases.yml
-ln -sf $dotfile_dir/glab-cli/config.yml $user_dir/.config/glab-cli/config.yml
+ln -sf $config_dir/glab-cli/aliases.yml $user_dir/.config/glab-cli/aliases.yml
+ln -sf $config_dir/glab-cli/config.yml $user_dir/.config/glab-cli/config.yml
 
 # i3
 mkdir -p $user_dir/.config/i3
-ln -sf $dotfile_dir/i3/config $user_dir/.config/i3/config
+ln -sf $config_dir/i3/config $user_dir/.config/i3/config
 
 # intelliJ
-ln -sf $dotfile_dir/ideavimrc $user_dir/.ideavimrc
+ln -sf $config_dir/ideavimrc $user_dir/.ideavimrc
 
 # kitty
 mkdir -p $user_dir/.config/kitty
-ln -sf $dotfile_dir/kitty/kitty.conf $user_dir/.config/kitty/kitty.conf
+ln -sf $config_dir/kitty/kitty.conf $user_dir/.config/kitty/kitty.conf
 
 # mpv
 mkdir -p $user_dir/.config/mpv
-ln -sf $dotfile_dir/mpv/mpv.conf $user_dir/.config/mpv/mpv.conf
+ln -sf $config_dir/mpv/mpv.conf $user_dir/.config/mpv/mpv.conf
 
 # nvim
 mkdir -p $user_dir/.config/nvim
-ln -sf $dotfile_dir/nvim/init.vim $user_dir/.config/nvim/init.vim
-ln -sf $dotfile_dir/nvim/coc-settings.json $user_dir/.config/nvim/coc-settings.json
+ln -sf $config_dir/nvim/init.vim $user_dir/.config/nvim/init.vim
+ln -sf $config_dir/nvim/coc-settings.json $user_dir/.config/nvim/coc-settings.json
 
 # picom
 mkdir -p $user_dir/.config/picom
-ln -sf $dotfile_dir/picom/picom.conf $user_dir/.config/picom/picom.conf
+ln -sf $config_dir/picom/picom.conf $user_dir/.config/picom/picom.conf
 
 # ranger
 mkdir -p $user_dir/.config/ranger
-ln -sf $dotfile_dir/ranger/ranger.rc $user_dir/.config/ranger/rc.conf
-ln -sf $dotfile_dir/ranger/ranger.commands $user_dir/.config/ranger/commands.py
+ln -sf $config_dir/ranger/ranger.rc $user_dir/.config/ranger/rc.conf
+ln -sf $config_dir/ranger/ranger.commands $user_dir/.config/ranger/commands.py
 
 # redshift
 mkdir -p $user_dir/.config/redshift
-ln -sf $dotfile_dir/redshift/redshift.conf $user_dir/.config/redshift/redshift.conf
+ln -sf $config_dir/redshift/redshift.conf $user_dir/.config/redshift/redshift.conf
 
 # todo.txt
 mkdir -p $user_dir/.todo
-ln -sf $dotfile_dir/todo/todo.cfg $user_dir/.todo/config
+ln -sf $config_dir/todo/todo.cfg $user_dir/.todo/config
 
 # spotify-tui
 mkdir -p $user_dir/.config/spotify-tui
-ln -sf $dotfile_dir/spotify-tui/config.yml $user_dir/.config/spotify-tui/config.yml
+ln -sf $config_dir/spotify-tui/config.yml $user_dir/.config/spotify-tui/config.yml
 
 # spotifyd
 mkdir -p $user_dir/.config/spotifyd
-ln -sf $dotfile_dir/spotifyd/spotifyd.conf $user_dir/.config/spotifyd/spotifyd.conf
+ln -sf $config_dir/spotifyd/spotifyd.conf $user_dir/.config/spotifyd/spotifyd.conf
+
+# ------------------------ .local ------------------------
 
 # plasma color scheme
 mkdir -p $user_dir/.local/share/color-schemes
-ln -sf $dotfile_dir/plasma/Kustom.colors $user_dir/.local/share/color-schemes/Kustom.colors
+ln -sf $config_dir/plasma/Kustom.colors $user_dir/.local/share/color-schemes/Kustom.colors
+
+# ------------------------ .mozilla ------------------------
 
 # firefox
 ln -sf $setup_dir/firefox/chrome/userChrome.css $ffProfilePath/chrome/userChrome.css
