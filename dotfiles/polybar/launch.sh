2be91ff5753155cf865d604fe1f28687e22f7b14
clemak27 <clemak27@mailbox.org>
2021-03-03 21:58:22 +0100

    ok

--- dotfiles/polybar/launch.sh
+++ dotfiles/polybar/launch.sh
@@ -3,10 +3,13 @@
 # Terminate already running bar instances
 killall -q polybar
 
+
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 polybar topbar 2>&1 | tee -a /tmp/polybar1.log & disown
+
+killall xembedsniproxy
 
 # displays=$(xrandr -q | grep " connected" | cut -d ' ' -f1)
 # exec echo $displays | grep -q "eDP1" &&
