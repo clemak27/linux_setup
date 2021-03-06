0fa17b42076851ca091a2eb870a6a0aaf0beb995
clemak27 <clemak27@mailbox.org>
2021-03-06 22:02:06 +0100

    rename dunstify tag

--- polybar/microphone.sh
+++ polybar/microphone.sh
@@ -25,10 +25,10 @@ toggle() {
 
   if [ "$MUTED" = "yes" ]; then
     pactl set-source-mute "$DEFAULT_SOURCE" 0
-    dunstify -h string:x-dunst-stack-tag:test -i audio-input-microphone Microphone unmuted
+    dunstify -h string:x-dunst-stack-tag:microphone -i audio-input-microphone Microphone unmuted
   else
     pactl set-source-mute "$DEFAULT_SOURCE" 1
-    dunstify -h string:x-dunst-stack-tag:test -i audio-input-microphone Microphone muted
+    dunstify -h string:x-dunst-stack-tag:microphone-i audio-input-microphone Microphone muted
   fi
 }
 
