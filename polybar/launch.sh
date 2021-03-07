e4af76c21609a7ce4bd4f21a7c8352750de29906
clemak27 <clemak27@mailbox.org>
2021-03-07 14:03:37 +0100

    launch polybar on all monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,7 +8,13 @@ polybar_path="$HOME/Projects/linux_setup/polybar/config"
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar --config=$polybar_path topbar 2>&1 | tee -a /tmp/polybar1.log & disown
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --config=$polybar_path --reload topbar &
+  done
+else
+  polybar --config=$polybar_path --reload topbar &
+fi
 
 # displays=$(xrandr -q | grep " connected" | cut -d ' ' -f1)
 # exec echo $displays | grep -q "eDP1" &&
