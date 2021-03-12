3bcaeff5282a81c5a6d33d1e12ec61f939d29d73
clemak27 <clemak27@mailbox.org>
2021-03-12 17:07:16 +0100

    parse commands

--- setup/random.sh
+++ setup/random.sh
@@ -1,24 +1,40 @@
-#!/bin/zsh
+#!/bin/bash
 
 set -uo pipefail
 
 config="./modules.json"
 
 function get_module() {
-  cat $config | jq -rc ".modules[] | select(.name == \"$1\") | ."
+  # cat $config | jq -rc ".modules[] | select(.name == \"$1\") | ."
+  echo ""
 }
 
-get_module plasma
-
 function get_packages() {
-  cat $config | jq -rc ".modules[] | select(.name == \"$1\") | .packages"
+  # cat $config | jq -rc ".modules[] | select(.name == \"$1\") | .packages"
   packages=($(<$config jq -r ".modules[] | select(.name == \"$1\") | .packages | @sh"))
   for i in "${packages[@]}"
   do
-    echo $i
+    # echo $i
     pacman -Ss $i
-    echo "exists"
+    # echo "exists"
   done
 }
 
+function get_commands() {
+  # cat $config | jq -rc ".modules[] | select(.name == \"$1\") | .commands"
+  commands=$(cat $config | jq -rc ".modules[] | select(.name == \"$1\") | .commands | @sh")
+  IFS="'" read -a pack <<< $commands
+
+  for i in "${pack[@]}"
+  do
+    l=${#i}
+    # shitty af
+    if [[ $l -gt 3 ]]; then
+      /bin/zsh -e -c $i
+    fi
+  done
+}
+
+get_module plasma
 get_packages plasma
+get_commands plasma
