3511f76245c5aa5644cc8da959e57c85dcfe880c
clemak27 <clemak27@mailbox.org>
2021-03-12 14:55:20 +0100

    add get_packages

--- setup/random.sh
+++ setup/random.sh
@@ -3,7 +3,16 @@
 config="./modules.json"
 
 function get_module() {
-  cat $config | jq -rc '.modules[] | select(.name == "plasma") | .'
+  cat $config | jq -rc ".modules[] | select(.name == \"$1\") | ."
 }
 
-get_module
+# get_module plasma
+
+function get_packages() {
+  cat $config | jq -rc ".modules[] | select(.name == \"$1\") | .packages"
+  packages=($(<$config jq -r ".modules[] | select(.name == \"$1\") | .packages | @sh"))
+  echo $packages
+}
+
+get_packages plasma
+
