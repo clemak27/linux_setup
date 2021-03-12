b35a2b1a032bf1944863db6207ba10bc98206d21
clemak27 <clemak27@mailbox.org>
2021-03-12 15:05:11 +0100

    use array

--- setup/random.sh
+++ setup/random.sh
@@ -1,18 +1,24 @@
 #!/bin/zsh
 
+set -uo pipefail
+
 config="./modules.json"
 
 function get_module() {
   cat $config | jq -rc ".modules[] | select(.name == \"$1\") | ."
 }
 
-# get_module plasma
+get_module plasma
 
 function get_packages() {
   cat $config | jq -rc ".modules[] | select(.name == \"$1\") | .packages"
   packages=($(<$config jq -r ".modules[] | select(.name == \"$1\") | .packages | @sh"))
-  echo $packages
+  for i in "${packages[@]}"
+  do
+    echo $i
+    pacman -Ss $i
+    echo "exists"
+  done
 }
 
 get_packages plasma
-
