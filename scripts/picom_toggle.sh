e054454197e277bda085481cbdd331a50f783ecf
clemak27 <clemak27@mailbox.org>
2021-04-02 16:58:39 +0200

    add notification to picom toggle

--- scripts/picom_toggle.sh
+++ scripts/picom_toggle.sh
@@ -4,6 +4,8 @@ pidof picom  >/dev/null
 
 if [[ $? -ne 0 ]] ; then
   picom -b
+  dunstify -h string:x-dunst-stack-tag:picom -i gtk-dialog-info picom "enabled"
 else
   killall -15 picom
+  dunstify -h string:x-dunst-stack-tag:picom -i gtk-dialog-info picom "disabled"
 fi
