65fca195ddd4a69ed612916b586e5d947cb71c77
clemak27 <clemak27@mailbox.org>
2021-06-28 20:47:24 +0200

    nvim - setup markdownlint

--- dotfiles/nvim/lua/nvim-lint-config.lua
+++ dotfiles/nvim/lua/nvim-lint-config.lua
@@ -4,8 +4,65 @@ local M = {}
 
 M.load = function()
 
+  -- set up markdownlint
+
+  local pattern = "[^:]+:(%d+):?(%d*) (MD%d+%/[%w-/]*) (.*)"
+
+  require('lint').linters.markdownlint = {
+    cmd = 'markdownlint',
+    stdin = false,
+    args = {},
+    stream = 'stderr',
+    ignore_exitcode = true,
+    parser = function(output)
+      local diagnostics = {}
+      for item in vim.gsplit(output, '\n') do
+        if item ~= '' and item ~= nil then
+          local line, column, lintCode, message = string.match(item, pattern)
+          if column == '' then
+            table.insert(diagnostics, {
+              source = 'markdownlint',
+              range = {
+                ['start'] = {
+                  line = tonumber(line) - 1,
+                  character = 0,
+                },
+                ['end'] = {
+                  line = tonumber(line) - 1,
+                  character = 0,
+                },
+              },
+              message = message,
+              code = lintCode,
+              severity = vim.lsp.protocol.DiagnosticSeverity.Hint,
+            })
+          else
+            table.insert(diagnostics, {
+              source = 'markdownlint',
+              range = {
+                ['start'] = {
+                  line = tonumber(line) - 1,
+                  character = tonumber(column) - 1,
+                },
+                ['end'] = {
+                  line = tonumber(line) - 1,
+                  character = tonumber(column),
+                },
+              },
+              message = message,
+              code = lintCode,
+              severity = vim.lsp.protocol.DiagnosticSeverity.Hint,
+            })
+          end
+        end
+      end
+      return diagnostics
+    end
+  }
+
   require('lint').linters_by_ft = {
-    go = {'revive'}
+    go = {'revive'},
+    markdown = {'markdownlint'},
   }
 
   vim.api.nvim_exec(
@@ -14,7 +71,6 @@ M.load = function()
     ]],
     false
   )
-
 
 end
 
