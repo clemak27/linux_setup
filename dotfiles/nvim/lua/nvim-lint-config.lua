bb90c796d0c2dae8a1fa17841cf1bd7d63f07c0e
clemak27 <clemak27@mailbox.org>
2021-07-03 16:39:10 +0200

    nvim - add eslint

--- dotfiles/nvim/lua/nvim-lint-config.lua
+++ dotfiles/nvim/lua/nvim-lint-config.lua
@@ -5,8 +5,7 @@ local M = {}
 M.load = function()
 
   -- set up markdownlint
-
-  local pattern = "[^:]+:(%d+):?(%d*) (MD%d+%/[%w-/]*) (.*)"
+  local mdl_pattern = "[^:]+:(%d+):?(%d*) (MD%d+%/[%w-/]*) (.*)"
 
   require('lint').linters.markdownlint = {
     cmd = 'markdownlint',
@@ -18,7 +17,7 @@ M.load = function()
       local diagnostics = {}
       for item in vim.gsplit(output, '\n') do
         if item ~= '' and item ~= nil then
-          local line, column, lintCode, message = string.match(item, pattern)
+          local line, column, lintCode, message = string.match(item, mdl_pattern)
           if column == '' then
             table.insert(diagnostics, {
               source = 'markdownlint',
@@ -60,12 +59,49 @@ M.load = function()
     end
   }
 
+  -- set up markdownlint
+  local esl_pattern = "[^:]+:(%d+):?(%d*): (.*) %[(.*)%]"
+
+  require('lint').linters.eslint = {
+    cmd = 'eslint',
+    stdin = false,
+    args = {"-f", "unix"},
+    stream = 'stdout',
+    ignore_exitcode = true,
+    parser = function(output)
+      local diagnostics = {}
+      for item in vim.gsplit(output, '\n') do
+        local line, column, message, lintCode = string.match(item, esl_pattern)
+        if line ~= nil and column ~= nil then
+          table.insert(diagnostics, {
+            source = 'eslint',
+            range = {
+              ['start'] = {
+                line = tonumber(line) - 1,
+                character = tonumber(column) - 1,
+              },
+              ['end'] = {
+                line = tonumber(line) - 1,
+                character = tonumber(column),
+              },
+            },
+            message = message,
+            code = lintCode,
+            severity = vim.lsp.protocol.DiagnosticSeverity.Hint,
+          })
+        end
+      end
+      return diagnostics
+    end
+  }
+
   require('lint').linters_by_ft = {
     go = {'revive'},
     markdown = {'markdownlint'},
     sh = {'shellcheck'},
     bash = {'shellcheck'},
-    zsh = {'shellcheck'}
+    zsh = {'shellcheck'},
+    javascript = {'eslint'}
   }
 
   vim.api.nvim_exec(
@@ -76,5 +112,4 @@ M.load = function()
   )
 
 end
-
 return M
