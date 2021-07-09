3bf85916cf41b741778de686f79751e95d0f42f7
clemak27 <clemak27@mailbox.org>
2021-07-09 14:48:41 +0200

    tmux - add dev session

---
+++ tmux_sessions/dev.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+tmux new-session -d -n root
+tmux rename-window root
+tmux set status off
+
+tmux selectp -t 1
+tmux split-window -v
+tmux resize-pane -t 2 -y 16
+
+tmux selectp -t 1
+tmux send-keys " unset TMUX" C-m
+tmux send-keys " tmux new -s dev -n projects -c ~/Projects" C-m
+
+tmux selectp -t 2
+tmux send-keys " unset TMUX" C-m
+tmux send-keys " tmux new -s term -n projects -c ~/Projects" C-m
+
+tmux attach-session -d
