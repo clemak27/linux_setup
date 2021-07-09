c03db9cfd485dabb311571491fff0dc54f1f8014
clemak27 <clemak27@mailbox.org>
2021-07-09 14:18:18 +0200

    scripts - rm nvim update script

--- scripts/update_nvim_nightly.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-curl -O -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-chmod u+x nvim.appimage
-mv nvim.appimage ~/Projects/linux_setup/other/nvim.appimage
