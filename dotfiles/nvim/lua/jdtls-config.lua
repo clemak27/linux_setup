969da6bb1bc8717a9f75ab17bb27f6f4ec81a6f5
clemak27 <clemak27@mailbox.org>
2021-08-09 22:33:31 +0200

    nvim - make jdtls config for every project

--- dotfiles/nvim/lua/jdtls-config.lua
+++ dotfiles/nvim/lua/jdtls-config.lua
@@ -9,7 +9,7 @@ M.load = function()
     [[
     augroup lsp
       au!
-      au FileType java lua require('jdtls').start_or_attach({cmd = {'java-lsp.sh'}})
+      au FileType java lua require('jdtls').start_or_attach({cmd = {'java-lsp.sh', '/home/clemens/.jdtls-workspace/' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')}})
     augroup end
     ]],
     false)
