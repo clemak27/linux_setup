404a4ad78dee3d8be1303819ddbd5c956243186e
clemak27 <clemak27@mailbox.org>
2021-08-26 17:01:38 +0200

    nvim - update setup script
    nvim - update script to dl latest jdtls
    
    nvim - update script to echo some output
    
    nvim - add helper to script if update is needed
    
    nvim - update script to check for jdtls update
    
    nvim - update script to check for new npm versions
    
    nvim - update script to check for new npm versions more dynamically
    
    nvim - update script to check for gopls update

--- scripts/setup_nvim_dev.sh
+++ scripts/setup_nvim_dev.sh
@@ -1,44 +1,128 @@
 #!/bin/sh
 
+# paths
 install_dir="$HOME/.local/bin/nvim"
 lsp_dir="$install_dir/lsp"
 dap_dir="$install_dir/dap"
 current_dir=$(pwd)
 
+# helper
+repo_updated() {
+  h1="$(git rev-parse HEAD)"
+  h2="$(git rev-parse @\{u\})"
+
+  if [ "$h1" = "$h2" ]
+  then
+    return 1
+  else
+    git pull
+    npm install
+    return 0
+  fi
+
+  unset h1
+  unset h2
+}
+
+update_node_package() {
+  pkg_name=$1
+  local=$(npm -g list --depth=0 | grep "$pkg_name" | awk -F@ '{ print $2}')
+  remote=$(npm v "$pkg_name" version)
+
+  if [ "$local" != "$remote" ]
+  then
+    npm i -g "$pkg_name"
+  else
+    echo "[$pkg_name] Already up to date."
+  fi
+
+  unset pkg_name
+  unset local
+  unset remote
+}
+
 # create folders
 mkdir -p "$lsp_dir"
 mkdir -p "$dap_dir"
 
+# --------------------------- update plugins ---------------------
+
+echo "Updating nvim plugins"
+nvim -c 'PlugUpgrade | PlugUpdate | qa'
+
 # --------------------------- lsp ------------------------------
 
+echo "Updating LSP server"
+
+# gopls
+go install golang.org/x/tools/gopls@latest
+
+# jdtls
 cd "$lsp_dir" || exit
-curl -O https://download.eclipse.org/jdtls/milestones/1.2.0/jdt-language-server-1.2.0-202106301459.tar.gz
-mkdir jdtls
-tar -xzvf jdt-language-server-1.2.0-202106301459.tar.gz -C ./jdtls
-rm jdt-language-server-1.2.0-202106301459.tar.gz
+if [ -d "jdtls" ]
+then
+  latest=$(curl --no-progress-meter https://download.eclipse.org/jdtls/snapshots/latest.txt)
+  current=$(cat current_jdtls.txt)
+  if [ "$latest" = "$current" ]
+  then
+    echo "[jdtls] Already up to date."
+  else
+    curl -O http://download.eclipse.org/jdtls/snapshots/jdt-language-server-latest.tar.gz
+    tar -xzvf jdt-language-server-latest.tar.gz -C ./jdtls
+    echo "$latest" > current_jdtls.txt
+  fi
+else
+  curl -O http://download.eclipse.org/jdtls/snapshots/jdt-language-server-latest.tar.gz
+  mkdir -p jdtls
+  tar -xzvf jdt-language-server-latest.tar.gz -C ./jdtls
+  echo "$latest" > current_jdtls.txt
+fi
+
+unset "$latest"
+unset "$current"
 
 # --------------------------- linter ---------------------------
-npm i -g markdownlint-cli
-npm i -g eslint
+
+echo "Updating linter"
+
+update_node_package markdownlint-cli
+update_node_package eslint
+
 go install github.com/mgechev/revive@latest
 paru -S shellcheck-bin
 
-# --------------------------- dap ------------------------------
+# --------------------------- dap --------------------------------
+
+echo "Updating Debug Adapters"
 
 # nodejs
 cd "$dap_dir" || exit
-git clone https://github.com/microsoft/vscode-node-debug2.git
-cd vscode-node-debug2 || exit
-npm install
-./node_modules/gulp/bin/gulp.js build
+if [ -d "vscode-node-debug2" ]
+then
+  cd vscode-node-debug2 || exit
+  git restore .
+  if repo_updated; then ./node_modules/gulp/bin/gulp.js build; else echo "[vscode-node-debug2] Already up to date."; fi
+else
+  git clone https://github.com/microsoft/vscode-node-debug2.git
+  cd vscode-node-debug2 || exit
+  npm install
+  ./node_modules/gulp/bin/gulp.js build
+fi
 
 # golang
 go install github.com/go-delve/delve/cmd/dlv@latest
 cd "$dap_dir" || exit
-git clone https://github.com/golang/vscode-go
-cd vscode-go || exit
-npm install
-npm run compile
+if [ -d "vscode-go" ]
+then
+  cd vscode-go || exit
+  git restore .
+  if repo_updated; then npm run compile; else echo "[vscode-go] Already up to date."; fi
+else
+  git clone https://github.com/golang/vscode-go
+  cd vscode-node-debug2 || exit
+  npm install
+  npm run compile
+fi
 
 # tmux integration
 cd "$current_dir" || exit
