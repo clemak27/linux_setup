3a1cff9b83d0ba2bcc146eb1f2ae16c7a680c977
clemak27 <clemak27@mailbox.org>
2021-08-27 17:09:11 +0200

    nvim - replace deprecated function in bufferline

--- dotfiles/nvim/lua/bufferline-config.lua
+++ dotfiles/nvim/lua/bufferline-config.lua
@@ -8,7 +8,6 @@ M.load = function()
     options = {
       numbers = "ordinal",
       number_style = "",
-      mappings = true,
       diagnostics = false,
       custom_filter = function(buf_number)
         -- filter out filetypes you don't want to see
@@ -26,6 +25,19 @@ M.load = function()
       always_show_bufferline = false,
     }
   }
+
+  local opt = {noremap = true, silent = true}
+
+  -- mappings
+  vim.api.nvim_set_keymap("n", "<Leader>1", [[<Cmd>BufferLineGoToBuffer 1<CR>]], opt)
+  vim.api.nvim_set_keymap("n", "<Leader>2", [[<Cmd>BufferLineGoToBuffer 2<CR>]], opt)
+  vim.api.nvim_set_keymap("n", "<Leader>3", [[<Cmd>BufferLineGoToBuffer 3<CR>]], opt)
+  vim.api.nvim_set_keymap("n", "<Leader>4", [[<Cmd>BufferLineGoToBuffer 4<CR>]], opt)
+  vim.api.nvim_set_keymap("n", "<Leader>5", [[<Cmd>BufferLineGoToBuffer 5<CR>]], opt)
+  vim.api.nvim_set_keymap("n", "<Leader>6", [[<Cmd>BufferLineGoToBuffer 6<CR>]], opt)
+  vim.api.nvim_set_keymap("n", "<Leader>7", [[<Cmd>BufferLineGoToBuffer 7<CR>]], opt)
+  vim.api.nvim_set_keymap("n", "<Leader>8", [[<Cmd>BufferLineGoToBuffer 8<CR>]], opt)
+  vim.api.nvim_set_keymap("n", "<Leader>9", [[<Cmd>BufferLineGoToBuffer 9<CR>]], opt)
 
 end
 
