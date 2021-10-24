a9c6936ce987e328eeff2989b65768966bc3f75d
clemak27 <clemak27@mailbox.org>
2021-10-24 14:29:30 +0200

    revert

--- configuration.nix
+++ configuration.nix
@@ -49,7 +49,7 @@
 
   # Enable the Plasma 5 Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.sddm.theme = "chili";
+  # services.xserver.displayManager.sddm.theme = "chili";
   services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
