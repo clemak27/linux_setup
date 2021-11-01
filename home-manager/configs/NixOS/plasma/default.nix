bd8f87b741dc646e2e24d465e8277897bd83db3c
clemak27 <clemak27@mailbox.org>
2021-11-01 15:48:22 +0100

    plasma - install event calendar with nix

--- home-manager/configs/NixOS/plasma/default.nix
+++ home-manager/configs/NixOS/plasma/default.nix
@@ -10,15 +10,14 @@ let
     '';
   };
   eventCalendar = pkgs.stdenv.mkDerivation {
-    name = "breeze-alpha-back";
+    name = "eventCalendar";
     src = fetchTarball {
       url = "https://github.com/Zren/plasma-applet-eventcalendar/archive/v75.tar.gz";
       sha256 = "0sq35jrgjv2pwy5l90nxxqqswp4ph1p2p1hj73x11i4yxx7fhmgd";
     };
     installPhase = ''
       mkdir -p $out
-      ls -hal .
-      cp -R ./package $out
+      cp -R ./package/* $out
     '';
   };
 in
@@ -40,8 +39,7 @@ in
     home.file.".xbindkeysrc".source = ./xbindkeysrc;
 
     home.file.".local/share/plasma/desktoptheme/breeze-alphablack".source = alphaBlack;
-    home.file."Desktop/test".source = eventCalendar; # org.kde.plasma.eventcalendar
-    # home.file.".local/share/plasma/plasmoids/test".source = eventCalendar; # org.kde.plasma.eventcalendar
+    home.file.".local/share/plasma/plasmoids/org.kde.plasma.eventcalendar".source = eventCalendar;
     home.file.".local/share/color-schemes/SkyBlue.colors".source = ./SkyBlue.colors;
   };
 }
