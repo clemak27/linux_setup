f2c5116d9a1cac139853c0462a25516bc9154a25
clemak27 <clemak27@mailbox.org>
2021-11-01 12:14:44 +0100

    create pipewire module

--- hosts/xps15/configuration.nix
+++ hosts/xps15/configuration.nix
@@ -13,6 +13,7 @@
     ./gpu.nix
 
     ../../modules/plasma.nix
+    ../../modules/pipewire.nix
     ../../modules/gaming.nix
     ../../modules/virt-manager.nix
     ../../modules/container.nix
@@ -62,28 +63,6 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
-
-  # Enable sound.
-  # Remove sound.enable or turn it off if you had it set previously, it seems to cause conflicts with pipewire
-  # sound.enable = false;
-
-  # rtkit is optional but recommended
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    media-session.enable = true;
-  };
-
-  # Enable blueooth
-  hardware.bluetooth.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.clemens = {
