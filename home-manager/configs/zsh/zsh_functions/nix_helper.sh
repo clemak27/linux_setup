6191e68258403cfaa061df4c3ccfc3a0276001f6
clemak27 <clemak27@mailbox.org>
2021-11-03 23:16:21 +0100

    update update_nix

--- home-manager/configs/zsh/zsh_functions/nix_helper.sh
+++ home-manager/configs/zsh/zsh_functions/nix_helper.sh
@@ -36,22 +36,3 @@ EOF
 nvim shell.nix
 
 }
-
-update_nix() {
-  echo "\e[1;36mUpdating nix channels\e[0m"
-  nix-channel --update
-  echo "\e[1;36mUpgrading nix-env\e[0m"
-  nix-env --upgrade
-  echo "\e[1;36mReloading home-manager config\e[0m"
-  home-manager switch
-
-  if [ -x $(which tldr) ] ; then
-    echo "\e[1;36mUpdating tealdeer cache\e[0m"
-    tldr --update
-  fi
-
-  if [ -x $(which nvim) ] ; then
-    echo "\e[1;36mUpdating additional nvim tools\e[0m"
-    update-nvim-dev
-  fi
-}
