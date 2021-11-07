d258f491708184282e7fd766349ffaf44fa4fa99
clemak27 <clemak27@mailbox.org>
2021-11-07 12:27:27 +0100

    add dconf settings

--- home-manager/configs/NixOS/gnome/default.nix
+++ home-manager/configs/NixOS/gnome/default.nix
@@ -29,28 +29,7 @@ in
       libreoffice
     ];
 
-    # don't know if that works, otherwise use
-    # gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "['<Super>7']"
-    dconf.settings = {
-      "org/gnome/desktop/wm/keybindings" = {
-        switch-to-workspace-1 = "['<Super>1']";
-        switch-to-workspace-2 = "['<Super>2']";
-        switch-to-workspace-3 = "['<Super>3']";
-        switch-to-workspace-4 = "['<Super>4']";
-        switch-to-workspace-5 = "['<Super>5']";
-        switch-to-workspace-6 = "['<Super>6']";
-        switch-to-workspace-7 = "['<Super>7']";
-        switch-to-workspace-8 = "['<Super>8']";
-      };
-      "org/gnome/desktop/interface" = {
-        gtk-theme = "Sweet-Dark";
-        icon-theme = "candy-icons";
-        cursor-theme = "Adwaita";
-      };
-      "org/gnome/desktop/wm/preferences" = {
-        theme = "Sweet-Dark";
-      };
-    };
+    dconf.settings = import ./dconf.nix;
 
     home.file.".local/share/icons/candy-icons".source = candyIcons;
   };
