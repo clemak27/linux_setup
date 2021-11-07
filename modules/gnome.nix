d53a7f809590d33da90dfc94bb408817a70bf300
clemak27 <clemak27@mailbox.org>
2021-11-07 11:34:03 +0100

    try to automate some gnome things

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -36,12 +36,4 @@
     style = "adwaita-dark";
   };
 
-  # gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
-  # gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
-  # gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
-  # gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
-  # gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
-  # gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
-  # gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "['<Super>7']"
-
 }
