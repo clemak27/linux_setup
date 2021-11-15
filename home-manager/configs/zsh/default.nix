165908bbf5cac2dbd6f51209e04ecc1c2dccb28f
clemak27 <clemak27@mailbox.org>
2021-11-15 09:10:37 +0100

    fix(home-manager/zsh): Use bat as MANPAGER

--- home-manager/configs/zsh/default.nix
+++ home-manager/configs/zsh/default.nix
@@ -53,7 +53,7 @@ in
         DIRENV_LOG_FORMAT = "";
         EDITOR = "nvim";
         GIT_SSH = "/usr/bin/ssh";
-        MANPAGER = "nvim -c 'set ft=man' -";
+        MANPAGER = "sh -c 'col -bx | bat -l man -p'";
         PATH = "$PATH:$HOME/.cargo/bin:$HOME/.go/bin:$HOME/.local/bin:$HOME/.local/bin/npm/bin";
         VISUAL = "nvim";
       };
