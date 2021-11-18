80922c813762df9ef395153b92bd48a02ed44101
clemak27 <clemak27@mailbox.org>
2021-11-18 19:37:29 +0100

    feat(home-manager/nvim): Use nvim-lint from nixpkgs

--- home-manager/configs/nvim/lint.nix
+++ home-manager/configs/nvim/lint.nix
@@ -1,19 +1,11 @@
 { config, lib, pkgs, ... }:
 let
   cfg = config.homecfg.nvim.lsp;
-  repo = "mfussenegger/nvim-lint";
 in
 {
   config = lib.mkIf (cfg.markdown || cfg.go || cfg.node || cfg.shell) {
     programs.neovim.plugins = with pkgs.vimPlugins; [
-      (pkgs.vimUtils.buildVimPluginFrom2Nix {
-        pname = "${lib.strings.sanitizeDerivationName repo}";
-        version = "master";
-        src = builtins.fetchGit {
-          url = "https://github.com/${repo}.git";
-          ref = "master";
-        };
-      })
+      nvim-lint
     ];
 
     homecfg.nvim.pluginSettings = ''
