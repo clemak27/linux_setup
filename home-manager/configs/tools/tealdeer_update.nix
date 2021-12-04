8e4b6c4cc1ccfddce1e19644c1ab3857818995cd
clemak27 <clemak27@mailbox.org>
2021-12-04 18:39:13 +0100

    feat(home-manager/tools): Add notifications to tealdeer-update timer

--- home-manager/configs/tools/tealdeer_update.nix
+++ home-manager/configs/tools/tealdeer_update.nix
@@ -6,8 +6,14 @@ in
   config = lib.mkIf (cfg.enable) {
     systemd.user.services.tealdeer-update-cache.Service = {
       Type = "oneshot";
+      ExecStartPre = ''
+        ${pkgs.zsh}/bin/zsh -c "notify-send 'Updating tealdeer-cache' 'Starting...' --icon=dialog-information"
+      '';
       ExecStart = ''
         ${pkgs.zsh}/bin/zsh -c "tldr --update"
+      '';
+      ExecStartPost = ''
+        ${pkgs.zsh}/bin/zsh -c "notify-send 'Updating tealdeer-cache' 'Done!' --icon=dialog-information"
       '';
     };
 
