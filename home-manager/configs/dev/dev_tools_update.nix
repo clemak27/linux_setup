0474cc9a93b09c729b9a6f32d6a536027f006700
clemak27 <clemak27@mailbox.org>
2021-12-04 18:41:02 +0100

    feat(home-manager/dev): Add notifications to dev-tool-update timer

--- home-manager/configs/dev/dev_tools_update.nix
+++ home-manager/configs/dev/dev_tools_update.nix
@@ -6,8 +6,14 @@ in
   config = lib.mkIf (cfg.enable) {
     systemd.user.services.update-dev-tools.Service = {
       Type = "oneshot";
+      ExecStartPre = ''
+        ${pkgs.zsh}/bin/zsh -c "notify-send 'Updating dev-tools' 'Starting...' --icon=dialog-information"
+      '';
       ExecStart = ''
         ${pkgs.zsh}/bin/zsh -c "/home/clemens/.nix-profile/bin/update-dev-tools"
+      '';
+      ExecStartPost = ''
+        ${pkgs.zsh}/bin/zsh -c "notify-send 'Updating dev-tools' 'Done!' --icon=dialog-information"
       '';
     };
 
