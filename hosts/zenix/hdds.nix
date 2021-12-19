9d159a067471c789afb003fdc9f842a6cf60f174
clemak27 <clemak27@mailbox.org>
2021-12-19 09:09:22 +0100

    chore(hosts/zenix): Install new SSD

--- hosts/zenix/hdds.nix
+++ hosts/zenix/hdds.nix
@@ -3,23 +3,7 @@
   # mount additional HDDs
   boot.supportedFilesystems = [ "ntfs" ];
   fileSystems."/home/clemens/Games" = {
-    device = "/dev/disk/by-uuid/5E2CCEED67691F72";
-    fsType = "ntfs";
-    options = [ "rw" "uid=1000" "gid=1000" "user" "exec" "umask=000" ];
-  };
-
-  fileSystems."/home/clemens/Archive" = {
-    device = "/dev/disk/by-uuid/48764082764072AC";
-    fsType = "ntfs";
-    options = [ "rw" "uid=1000" "gid=1000" "user" "exec" "umask=000" ];
-  };
-  fileSystems."/home/clemens/Videos" = {
-    device = "/dev/disk/by-uuid/ACE41486E4145544";
-    fsType = "ntfs";
-    options = [ "rw" "uid=1000" "gid=1000" "user" "exec" "umask=000" ];
-  };
-  fileSystems."/home/clemens/.ssd_games" = {
-    device = "/dev/disk/by-uuid/692ccaac-ce21-45aa-bdd4-7d7ea5b2e497";
+    device = "/dev/disk/by-uuid/67de20f1-9d79-42ba-aec1-2ff30faf5dd9";
     fsType = "ext4";
     options = [ "defaults" ];
   };
