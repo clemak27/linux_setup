84f05526f259e795015bba5af4073e43c0cbb2a0
clemak27 <clemak27@mailbox.org>
2021-12-19 13:53:14 +0100

    feat(hosts/e470): Init config for e470

---
+++ hosts/e470/home.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [
+    ../../home-manager/homecfg.nix
+    ./duckdns.nix
+  ];
+
+  homecfg = {
+    git = {
+      enable = true;
+      user = "clemak27";
+      email = "clemak27@mailbox.org";
+      tea = false;
+      gh = false;
+      glab = false;
+    };
+    nvim = {
+      enable = true;
+    };
+    tmux = {
+      enable = true;
+    };
+    tools = {
+      enable = true;
+    };
+    zsh = {
+      enable = true;
+    };
+  };
+
+  home.packages = [
+    pkgs.gcc
+  ];
+
+  home.file."docker-compose.yml".source = ./docker-compose.yml;
+  home.file.".env".source = ./env;
+}
