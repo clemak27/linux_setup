2767184e16dac25ef151368f3de03a4b034d03aa
clemak27 <clemak27@mailbox.org>
2021-12-21 12:01:44 +0100

    chore(modules/automation): Remove flatpak update

--- modules/automation.nix
+++ modules/automation.nix
@@ -3,7 +3,6 @@ let
   updateSystem = pkgs.writeShellScriptBin "update-system" ''
     sudo nixos-rebuild switch --upgrade
     nix-collect-garbage
-    flatpak update
     nvim -c 'PlugUpgrade | PlugUpdate | qa'
     tldr --update
   '';
