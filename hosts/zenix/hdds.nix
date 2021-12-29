536fe7a9241a73a99ec56b0b9060ff64f8d42a64
clemak27 <clemak27@mailbox.org>
2021-12-29 11:16:09 +0100

    feat(hosts/zenix): Mount nfs volumes

--- hosts/zenix/hdds.nix
+++ hosts/zenix/hdds.nix
@@ -7,4 +7,16 @@
     fsType = "ext4";
     options = [ "defaults" ];
   };
+
+  fileSystems."/home/clemens/Archive" = {
+    device = "192.168.0.30:/archive";
+    fsType = "nfs";
+    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600"];
+  };
+
+  fileSystems."/home/clemens/Videos" = {
+    device = "192.168.0.30:/videos";
+    fsType = "nfs";
+    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600"];
+  };
 }
