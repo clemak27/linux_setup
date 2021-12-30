fc2ce15bfad58d61dfde75e84af8a16b4ca038e5
clemak27 <clemak27@mailbox.org>
2021-12-30 11:02:27 +0100

    feat(hosts/e470): Add deemix to traefik

--- hosts/e470/container/deemix.nix
+++ hosts/e470/container/deemix.nix
@@ -27,6 +27,14 @@ in
         ];
         extraOptions = [
           "--network=web"
+          "--label=traefik.enable=true"
+          "--label=traefik.http.routers.${service-name}-router.entrypoints=https"
+          "--label=traefik.http.routers.${service-name}-router.rule=Host(`${service-name}.hemvist.duckdns.org`)"
+          "--label=traefik.http.routers.${service-name}-router.tls=true"
+          "--label=traefik.http.routers.${service-name}-router.tls.certresolver=letsEncrypt"
+          # HTTP Services
+          "--label=traefik.http.routers.${service-name}-router.service=${service-name}-service"
+          "--label=traefik.http.services.${service-name}-service.loadbalancer.server.port=${service-port}"
         ];
       };
     };
