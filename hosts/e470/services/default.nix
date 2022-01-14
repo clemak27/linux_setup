95121bb6d3a5505c1d901e20d35cec2e05566c36
clemak27 <clemak27@mailbox.org>
2022-01-14 15:10:38 +0100

    chore(hosts/e470): Delete comment

--- hosts/e470/services/default.nix
+++ hosts/e470/services/default.nix
@@ -17,26 +17,4 @@
   virtualisation.oci-containers = {
     backend = "docker";
   };
-
-# systemd.services.init-docker-network-backend = {
-#   description = "Create backend network.";
-#   after = [ "network.target" ];
-#   wantedBy = [ "multi-user.target" ];
-  
-#   serviceConfig.Type = "oneshot";
-#    script =
-#      let
-#        dockercli = "${config.virtualisation.docker.package}/bin/docker";
-#      in ''
-#        # Put a true at the end to prevent getting non-zero return code, which will
-#        # crash the whole service.
-#        check=$(${dockercli} network ls | grep "backend" || true)
-#        if [ -z "$check" ]; then
-#          ${dockercli} network create backend --internal
-#        else
-#          echo "backend network already exists in docker"
-#        fi
-#      '';
-# };
-
 }
