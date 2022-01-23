019b0dd070eebe849a4fdf30a194758d08cf7c69
clemak27 <clemak27@mailbox.org>
2022-01-23 13:14:14 +0100

    feat(general): Try to unmake devshell

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-21.11";
 
     home-manager = {
       url = "github:nix-community/home-manager/master";
@@ -11,7 +11,7 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, home-manager, sops-nix, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, sops-nix, flake-utils }:
     # flake-utils.lib.eachDefaultSystem (system:
     #   let
     #     devpkgs = nixpkgs.legacyPackages.${system};
@@ -32,6 +32,25 @@
           home-manager.nixosModules.home-manager
           sops-nix.nixosModules.sops
           ./hosts/xps15/configuration.nix
+        ];
+      };
+
+      nixosConfigurations.e470 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ({ config, pkgs, ... }:
+            let
+              overlay-stable = final: prev: {
+                stable = nixpkgs-stable.legacyPackages.x86_64-linux;
+              };
+            in
+            {
+              nixpkgs.overlays = [ overlay-stable ];
+            }
+          )
+          home-manager.nixosModules.home-manager
+          sops-nix.nixosModules.sops
+          ./hosts/e470/configuration.nix
         ];
       };
 
