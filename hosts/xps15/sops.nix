19d2c43301d2faa3fdab060ed3f17c4cb2c2cefa
clemak27 <clemak27@mailbox.org>
2022-01-27 22:41:14 +0100

    fix(hosts/xps15): Flake support

--- hosts/xps15/sops.nix
+++ hosts/xps15/sops.nix
@@ -1,7 +1,5 @@
 { config, lib, pkgs, ... }:
 {
-  imports = [ <sops-nix/modules/sops> ];
-
   # This will add secrets.yml to the nix store
   # You can avoid this by adding a string to the full path instead, i.e.
   # sops.defaultSopsFile = "/root/.sops/secrets/example.yaml";
