d7ecd611f75940c3e2bd865823c12689c00d61d4
clemak27 <clemak27@mailbox.org>
2022-02-19 18:44:05 +0100

    add icon theme

--- setup/1_applications.sh
+++ setup/1_applications.sh
@@ -7,11 +7,13 @@ flatpak install -y flathub org.mozilla.firefox org.keepassxc.KeePassXC org.kde.k
 mkdir -p /home/clemens/.config/alacritty
 ln -sf /home/clemens/Projects/linux_setup/dotfiles/alacritty.yml /home/clemens/.config/alacritty/alacritty.yml
 
-
 # install fonts
 curl -O https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
 mkdir -p /home/clemens/.local/share/fonts
 unzip FiraCode.zip -d /home/clemens/.local/share/fonts
+
+# install icon theme
+wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.icons" sh
 
 # workaround for alacritty WL issue
 cp /usr/share/applications/Alacritty.desktop ~/.local/share/applications/
