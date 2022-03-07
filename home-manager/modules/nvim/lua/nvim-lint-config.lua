18113c2eac07c573d5ed1f2e3ab2b1bf68cc998c
clemak27 <clemak27@mailbox.org>
2022-03-07 09:57:32 +0100

    chore(home-manager/nvim): Use provided eslint linter

--- home-manager/modules/nvim/lua/nvim-lint-config.lua
+++ home-manager/modules/nvim/lua/nvim-lint-config.lua
@@ -4,23 +4,6 @@ local M = {}
 
 M.load = function()
 
-  -- set up eslint
-  local pattern = [[%s*(%d+):(%d+)%s+(%w+)%s+([%w%s]+)%s+(.*)]]
-  local groups = { 'line', 'start_col', 'severity', 'message', 'code' }
-  local severity_map = {
-    ['error'] = vim.lsp.protocol.DiagnosticSeverity.Error,
-    ['warn'] = vim.lsp.protocol.DiagnosticSeverity.Warning,
-  }
-
-  require('lint').linters.eslint = {
-    cmd = 'eslint',
-    args = {},
-    stdin = false,
-    stream = 'stdout',
-    ignore_exitcode = true,
-    parser = require('lint.parser').from_pattern(pattern, groups, severity_map, { ['source'] = 'eslint' }),
-  }
-
   require('lint').linters_by_ft = {
     go = {'revive'},
     markdown = {'markdownlint'},
