0a4111e785662d13e85fb4cd940a5967b7542ea8
clemak27 <clemak27@mailbox.org>
2022-03-27 21:37:03 +0200

    feat(home-manager/nvim): Add brackets when completing functions

--- home-manager/modules/nvim/lua/nvim-cmp-config.lua
+++ home-manager/modules/nvim/lua/nvim-cmp-config.lua
@@ -7,7 +7,9 @@ M.load = function()
   vim.o.completeopt = "menuone,noselect"
   vim.cmd('set shortmess+=c')
 
+  local cmp_autopairs = require('nvim-autopairs.completion.cmp')
   local cmp = require'cmp'
+  cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
 
   cmp.setup({
     snippet = {
