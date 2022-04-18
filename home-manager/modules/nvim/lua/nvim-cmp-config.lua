23ed7c2a8a7c56b28fb2b5e25c36d5451abc5382
clemak27 <clemak27@mailbox.org>
2022-04-18 13:38:24 +0200

    feat(home-manager/nvim): Update cmp config to better support snippets

--- home-manager/modules/nvim/lua/nvim-cmp-config.lua
+++ home-manager/modules/nvim/lua/nvim-cmp-config.lua
@@ -1,4 +1,4 @@
--- ---------------------------------------- nvim-compe -------------------------------------------------------
+-- ---------------------------------------- nvim-cmp -------------------------------------------------------
 
 local M = {}
 
@@ -11,6 +11,15 @@ M.load = function()
   local lspkind = require('lspkind')
   local cmp = require'cmp'
   cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+
+  local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  end
+
+  local feedkey = function(key, mode)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+  end
 
   cmp.setup({
     snippet = {
@@ -27,20 +36,24 @@ M.load = function()
         c = cmp.mapping.close(),
       }),
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
-      ['<tab>'] = cmp.mapping(function(fallback)
+      ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
+        elseif vim.fn["vsnip#available"](1) == 1 then
+          feedkey("<Plug>(vsnip-expand-or-jump)", "")
+        elseif has_words_before() then
+          cmp.complete()
         else
-          fallback()
+          fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
         end
-      end, { 'i',  's' }),
-      ['<s-tab>'] = cmp.mapping(function(fallback)
+      end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function()
         if cmp.visible() then
           cmp.select_prev_item()
-        else
-          fallback()
+        elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+          feedkey("<Plug>(vsnip-jump-prev)", "")
         end
-      end, { 'i',  's' }),
+      end, { "i", "s" }),
     },
     formatting = {
       format = lspkind.cmp_format({
@@ -54,8 +67,8 @@ M.load = function()
       { name = 'path' },
       { name = 'treesitter' },
     }, {
-      { name = 'buffer' },
-    })
+        { name = 'buffer' },
+      })
   })
 
   -- Use buffer source for `/`.
