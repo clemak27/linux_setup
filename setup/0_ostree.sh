da41966e9fd2ea7f2c794467d51dc5d9fc709122
clemak27 <clemak27@mailbox.org>
2022-11-17 20:12:31 +0100

    chore: remove unused overlays

--- setup/0_ostree.sh
+++ setup/0_ostree.sh
@@ -4,13 +4,10 @@ rpm-ostree install \
   alacritty \
   gnome-shell-extension-appindicator \
   gnome-shell-extension-gsconnect \
-  libffi3.1-3.1-32.fc36.x86_64 \
   gnome-tweaks \
   vim \
   syncthing \
   wireguard-tools \
-  xclip \
-  xprop \
-  xrandr
+  xprop
 rpm-ostree override remove firefox firefox-langpacks
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
