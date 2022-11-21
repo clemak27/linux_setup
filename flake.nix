44c6192eddfb70abbe8a29571473dcf18359dc1a
clemak27 <clemak27@mailbox.org>
2022-11-21 17:05:53 +0100

    feat: add stable channel as fallback

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-22.05";
+
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -10,13 +12,19 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, sops-nix, homecfg }:
+  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, homecfg }:
+    let
+      overlay-stable = final: prev: {
+        stable = self.inputs.nixpkgs-stable.legacyPackages.x86_64-darwin;
+      };
+    in
     {
       homeConfigurations = {
         "clemens@toolbox" = home-manager.lib.homeManagerConfiguration {
           pkgs = self.inputs.nixpkgs.legacyPackages.x86_64-linux;
           modules = [
             ({ config, pkgs, ... }: {
+              nixpkgs.overlays = [ overlay-stable ];
               nix.registry.nixpkgs.flake = self.inputs.nixpkgs;
             })
             homecfg.nixosModules.homecfg
