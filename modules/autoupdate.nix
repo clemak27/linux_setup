48f7b09e13d2ea4b3ffd28b8251e158ee2f6f7cf
clemak27 <clemak27@mailbox.org>
2023-05-30 20:52:38 +0200

    feat: add module for autoupdates

---
+++ modules/autoupdate.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+{
+  systemd.services.autoupdate = {
+    description = "NixOS Autoupdate Service";
+    wantedBy = [ "multi-user.target" ];
+    path = [
+      pkgs.busybox
+      pkgs.coreutils
+      pkgs.flatpak
+      pkgs.git
+      pkgs.home-manager
+      pkgs.jq
+      pkgs.libnotify
+      pkgs.neovim
+    ];
+    serviceConfig = {
+      ExecStart = "${pkgs.bash}/bin/bash /home/clemens/Projects/linux_setup/modules/autoupdate.sh";
+      Type = "oneshot";
+      User = "clemens";
+      Group = "users";
+      Environment = "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus";
+    };
+  };
+}
