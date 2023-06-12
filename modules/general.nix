7353634fc1698fda77e4ec828927bf7240dea1b5
clemak27 <clemak27@mailbox.org>
2023-06-12 22:11:53 +0200

    chore: remove deprecated option

--- modules/general.nix
+++ modules/general.nix
@@ -15,7 +15,6 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
   boot.loader.grub.device = "nodev";
   boot.loader.grub.efiSupport = true;
   boot.loader.efi.canTouchEfiVariables = true;
