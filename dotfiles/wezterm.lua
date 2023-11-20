98d40efebb512930e73235cfa9a8fd080a4cca1a
clemak27 <clemak27@mailbox.org>
2023-11-20 16:43:01 +0100

    chore: reorder wezterm config

--- dotfiles/wezterm.lua
+++ dotfiles/wezterm.lua
@@ -5,31 +5,34 @@ local wezterm = require("wezterm")
 return {
   default_prog = { "/home/clemens/.nix-profile/bin/zsh" },
 
+  -- leads to weirdly bold font
+  -- front_end = "WebGpu",
+  enable_wayland = true,
+  animation_fps = 144,
+
   color_scheme = "Catppuccin Mocha",
   window_background_opacity = 1.0,
-  window_close_confirmation = "NeverPrompt",
+  xcursor_theme = "Catppuccin-Mocha-Dark-Cursors",
 
   font = wezterm.font_with_fallback({
     "JetbrainsMono Nerd Font",
     "Noto Color Emoji",
   }),
-
   harfbuzz_features = { "calt=0", "clig=0", "liga=0" }, -- disable ligatures
   warn_about_missing_glyphs = false,
+  font_size = 10.0,
+  line_height = 0.9,
 
   enable_tab_bar = false,
+  window_close_confirmation = "NeverPrompt",
+  audible_bell = "Disabled",
+  hide_mouse_cursor_when_typing = false,
+
+  window_decorations = "RESIZE",
   window_padding = {
     left = 0,
     right = 0,
     top = 0,
     bottom = 0,
   },
-
-  audible_bell = "Disabled",
-  hide_mouse_cursor_when_typing = false,
-  enable_wayland = true,
-  xcursor_theme = "Catppuccin-Mocha-Dark-Cursors",
-  font_size = 10.0,
-  line_height = 0.9,
-  window_decorations = "RESIZE",
 }
