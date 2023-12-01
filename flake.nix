c027b23ac0a5ed1b3febb3f8c196e01c15d44547
clemak27 <clemak27@mailbox.org>
2023-12-01 12:18:24 +0100

    feat: update nixpkgs-stable to 23.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
 
     home-manager = {
       url = "github:nix-community/home-manager/master";
