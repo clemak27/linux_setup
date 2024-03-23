1d456c54a95dd76775f32bd94e09563758c1f5ab
clemak27 <clemak27@mailbox.org>
2024-03-23 11:57:16 +0100

    fix: add k8s dunction only if kubectl exists

--- dotfiles/zsh/zsh_functions/k8s.sh
+++ dotfiles/zsh/zsh_functions/k8s.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-function kj() { kubectl "$@" -o json | jq; }
-compdef kj=kubectl
-function ky() { kubectl "$@" -o yaml | bat -p -P --language=yaml; }
-compdef ky=kubectl
-function wk() { viddy --no-title kubecolor --force-colors "$@"; }
-compdef wk=kubectl
+if command -v kubectl &> /dev/null; then
+  function kj() { kubectl "$@" -o json | jq; }
+  compdef kj=kubectl
+  function ky() { kubectl "$@" -o yaml | bat -p -P --language=yaml; }
+  compdef ky=kubectl
+  function wk() { viddy --no-title kubecolor --force-colors "$@"; }
+  compdef wk=kubectl
+fi
