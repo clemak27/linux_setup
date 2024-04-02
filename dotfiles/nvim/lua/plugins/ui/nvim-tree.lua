06c177d759322854485ccc3efbc986e967ae11d3
clemak27 <clemak27@mailbox.org>
2024-04-02 19:06:27 +0200

    fix(nvim): improve session saving with nvim-tree

--- dotfiles/nvim/lua/plugins/ui/nvim-tree.lua
+++ dotfiles/nvim/lua/plugins/ui/nvim-tree.lua
@@ -1,6 +1,18 @@
 return {
   "nvim-tree/nvim-tree.lua",
   config = function()
+    vim.api.nvim_create_autocmd({ "BufEnter" }, {
+      pattern = "NvimTree*",
+      callback = function()
+        local api = require("nvim-tree.api")
+        local view = require("nvim-tree.view")
+
+        if not view.is_visible() then
+          api.tree.open()
+        end
+      end,
+    })
+
     require("nvim-tree").setup({
       sort_by = "case_sensitive",
       view = {
