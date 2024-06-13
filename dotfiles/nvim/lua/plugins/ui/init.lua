998f2e8d6def9979b1525eed18e9e125df0b8b57
clemak27 <clemak27@mailbox.org>
2024-06-13 09:17:33 +0200

    fix(nvim): adapt highlight color icon

--- dotfiles/nvim/lua/plugins/ui/init.lua
+++ dotfiles/nvim/lua/plugins/ui/init.lua
@@ -14,8 +14,8 @@ return {
     config = function()
       require("nvim-highlight-colors").setup({
         render = "virtual",
-        virtual_symbol = "■",
-        virtual_symbol_prefix = "",
+        virtual_symbol = "󰝤",
+        virtual_symbol_prefix = " ",
         virtual_symbol_suffix = " ",
         virtual_symbol_position = "inline",
       })
