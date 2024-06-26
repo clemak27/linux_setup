965883920a3aa8ca76db951a2ee0c72eb6ce871a
clemak27 <clemak27@mailbox.org>
2024-06-26 22:27:35 +0200

    feat: remove fedora config

--- dotfiles/nvim/lua/plugins/lsp/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-  require("plugins.lsp.conform"),
-  require("plugins.lsp.mason"),
-  require("plugins.lsp.neotest"),
-  require("plugins.lsp.nvim-lint"),
-  require("plugins.lsp.nvim-dap"),
-  require("plugins.lsp.nvim-lspconfig"),
-  require("plugins.lsp.trouble"),
-}
