43bb814687d860123463c5472b8e66985a62dcc3
clemak27 <clemak27@mailbox.org>
2024-07-01 16:35:23 +0200

    feat: add setup for deck

--- flake.nix
+++ flake.nix
@@ -88,6 +88,50 @@
       #   ];
       # };
 
+      homeConfigurations = {
+        deck = home-manager.lib.homeManagerConfiguration {
+          pkgs = legacyPkgs;
+          modules = [
+            nixModule
+            homecfg.hmModules.homecfg
+            nix-index-database.hmModules.nix-index
+            ({ config, pkgs, ... }: {
+              home = {
+                username = "deck";
+                homeDirectory = "/home/deck";
+                stateVersion = "24.05";
+                packages = [
+                  pkgs.dsda-doom
+                ];
+              };
+              news.display = "silent";
+              homecfg = {
+                git.enable = true;
+                tools.enable = true;
+                zsh.enable = true;
+              };
+              services.syncthing.enable = true;
+              programs.zsh = {
+                shellAliases = builtins.listToAttrs (
+                  [
+                    { name = "hms"; value = "git -C /home/deck/Projects/linux_setup pull --rebase && home-manager switch --flake /home/deck/Projects/linux_setup"; }
+                  ]
+                );
+
+                initExtra = ''
+                  if [ -z "$NIX_PROFILES" ]; then
+                    . $HOME/.nix-profile/etc/profile.d/nix.sh
+                  fi
+                  # export correct shell
+                  export SHELL="$HOME/.nix-profile/bin/zsh"
+                  export GIT_SSH="/usr/bin/ssh";
+                '';
+              };
+            })
+          ];
+        };
+      };
+
       devShell.x86_64-linux =
         legacyPkgs.mkShell {
           packages = with legacyPkgs; [
