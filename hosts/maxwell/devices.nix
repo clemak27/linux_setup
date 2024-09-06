746c233065e4e6bd6daf05e49bedb34fa0380412
clemak27 <clemak27@mailbox.org>
2024-09-06 12:23:13 +0200

    fix(maxwell): remove useless/broken openrgb udev rule

--- hosts/maxwell/devices.nix
+++ hosts/maxwell/devices.nix
@@ -1,10 +1,4 @@
 { pkgs, ... }:
-let
-  openrgbUdev = pkgs.fetchurl {
-    url = "https://openrgb.org/releases/release_0.9/60-openrgb.rules";
-    hash = "sha256-txvs6bHped0IVUoktwA7bXu2136lVOJhNUI/hMGvqzg=";
-  };
-in
 {
   environment.systemPackages = with pkgs; [
     piper
@@ -13,9 +7,5 @@ in
   services.ratbagd.enable = true;
 
   services.hardware.openrgb.enable = true;
-  services.udev.extraRules = ''
-    ${openrgbUdev}
-  '';
-
   hardware.keyboard.qmk.enable = true;
 }
