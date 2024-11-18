aaa41b2c1359cd091a6bdb036543617d0471ce3b
clemak27 <clemak27@mailbox.org>
2024-11-18 18:38:39 +0100

    fix(gaming): add additional version of mangohud
    this is needed so it works in lutris

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -2,6 +2,7 @@
 {
 
   services.flatpak.packages = [
+    "org.freedesktop.Platform.VulkanLayer.MangoHud//23.08"
     "org.freedesktop.Platform.VulkanLayer.MangoHud//24.08"
     "org.freedesktop.Platform.VulkanLayer.gamescope//24.08"
   ];
