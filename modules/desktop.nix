db7adf46c95c994c039501897f79d579b1fd245b
clemak27 <clemak27@mailbox.org>
2024-12-03 22:10:43 +0100

    fix(desktop): remove some unused gaming packages

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -46,14 +46,10 @@
     "org.freedesktop.Platform.VulkanLayer.MangoHud//24.08"
     "org.freedesktop.Platform.VulkanLayer.gamescope//24.08"
   ];
-
   hardware.steam-hardware.enable = true;
-
   environment.systemPackages = with pkgs; [
     gamemode
 
-    prismlauncher
-    gzdoom
     dsda-doom
   ];
 }
