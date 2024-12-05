a46d402362b4b2bdf4604bda369f083b5ebb0897
clemak27 <clemak27@mailbox.org>
2024-12-05 21:58:59 +0100

    chore(maxwell): reorder files

--- hosts/maxwell/configuration.nix
+++ hosts/maxwell/configuration.nix
@@ -1,10 +1,7 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./hardware-configuration.nix
-    ./mounts.nix
-
-    ./devices.nix
   ];
 
   networking.hostName = "maxwell";
@@ -16,6 +13,31 @@
   };
 
   services.xserver.xkb.layout = "us";
+
+  environment.systemPackages = with pkgs; [
+    nfs-utils
+    piper
+  ];
+
+  # devices
+  services.ratbagd.enable = true;
+  services.hardware.openrgb.enable = true;
+  hardware.keyboard.qmk.enable = true;
+
+  # mounts
+  fileSystems = {
+    "/home/clemens/Games" = {
+      device = "/dev/disk/by-uuid/1d007512-92c1-40ba-8863-f50589b88437";
+      fsType = "btrfs";
+      options = [ "compress=zstd" ];
+    };
+
+    # "/home/clemens/nfs/media" = {
+    #   device = "192.168.178.100:/media";
+    #   fsType = "nfs";
+    #   options = [ "x-systemd.automount" "_netdev" "x-systemd.idle-timeout=60" "noauto" ];
+    # };
+  };
 
   system.stateVersion = "24.05";
 }
