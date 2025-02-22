25f5b1f366bff3cf400e4e3cd903e0885404a4eb
clemak27 <clemak27@mailbox.org>
2025-02-22 08:47:57 +0100

    fix(hosts): remove lagrange

--- flake.nix
+++ flake.nix
@@ -129,13 +129,6 @@
         ];
       };
 
-      nixosConfigurations.lagrange = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = desktopModules ++ [
-          ./hosts/lagrange/configuration.nix
-        ];
-      };
-
       nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
         system = "aarch64-linux";
         modules = [
