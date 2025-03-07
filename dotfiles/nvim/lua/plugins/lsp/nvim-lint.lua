17433124ecffc54fb02aaa543c9114a69c6a0ca2
clemak27 <clemak27@mailbox.org>
2025-03-07 19:57:54 +0100

    Revert "feat: remove fedora config"
    This reverts commit 46f0cc93bc908beb37cac2aa3be504aeb03a6400.

---
+++ dotfiles/nvim/lua/plugins/lsp/nvim-lint.lua
@@ -0,0 +1,38 @@
+-- ---------------------------------------- nvim-lint --------------------------------------------------------
+return {
+  {
+    "mfussenegger/nvim-lint",
+    config = function()
+      local markdownlint = require("lint").linters.markdownlint
+      local efm = "stdin:%l:%c %m,stdin:%l %m"
+      markdownlint.args = { "--stdin" }
+      markdownlint.stdin = true
+      markdownlint.append_fname = false
+      markdownlint.parser = require("lint.parser").from_errorformat(efm, {
+        source = "markdownlint",
+        severity = vim.diagnostic.severity.WARN,
+      })
+
+      require("lint").linters_by_ft = {
+        markdown = { "markdownlint" },
+        dockerfile = { "hadolint" },
+        yaml = { "yamllint" },
+      }
+
+      local gradleRoot = vim.fs.root(0, "build.gradle")
+      if gradleRoot ~= nil then
+        local checkstyleCfg = gradleRoot .. "/config/checkstyle/checkstyle.xml"
+        if vim.fs.find(checkstyleCfg, {}) then
+          require("lint").linters.checkstyle.args = { "-c", checkstyleCfg }
+          require("lint").linters_by_ft.java = { "checkstyle" }
+        end
+      end
+
+      vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave", "TextChanged" }, {
+        callback = function()
+          require("lint").try_lint()
+        end,
+      })
+    end,
+  },
+}
---
+++ dotfiles/nvim/lua/plugins/snippets.lua
@@ -0,0 +1,6 @@
+-- ---------------------------------------- snippets --------------------------------------------------------
+return {
+  "hrsh7th/vim-vsnip",
+  "hrsh7th/vim-vsnip-integ",
+  "rafamadriz/friendly-snippets",
+}
