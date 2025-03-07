4c9523f76dc32094a5f3c39b4496ccc9482ba621
clemak27 <clemak27@mailbox.org>
2025-03-07 23:17:29 +0100

    fix: applications

--- customization.mk
+++ customization.mk
@@ -4,7 +4,7 @@ JBMONO_VERSION=3.0.2
 ADW_GTK3_VERSION=5.3
 
 .PHONY: customization
-customization: jbMonoFont adwGtkTheme papirusIconTheme catppuccinCursor
+customization: jbMonoFont adwGtkTheme papirusIconTheme
 
 .PHONY: jbMonoFont
 jbMonoFont: jbMonoFont/download jbMonoFont/cleanup
@@ -27,16 +27,9 @@ adwGtkTheme:
 	mkdir -p tmp $$HOME/.local/share/themes
 	curl -Lo tmp/adw-gtk3.tar.xz --url https://github.com/lassekongo83/adw-gtk3/releases/download/v$(ADW_GTK3_VERSION)/adw-gtk3v$(ADW_GTK3_VERSION).tar.xz
 	tar xf tmp/adw-gtk3.tar.xz --directory $$HOME/.local/share/themes
-	flatpak install -y com.github.GradienceTeam.Gradience org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark
+	flatpak install -y org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark
 	flatpak override --user --filesystem=xdg-config/gtk-3.0 --filesystem=xdg-config/gtk-4.0
 
 .PHONY: papirusIconTheme
 papirusIconTheme:
 	wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$$HOME/.local/share/icons" sh
-
-.PHONY: catppuccinCursor
-catppuccinCursor:
-	mkdir -p tmp $$HOME/.local/share/icons
-	rm -rf tmp/catppuccinCursor
-	git clone https://github.com/catppuccin/cursors.git tmp/catppuccinCursor
-	unzip -o tmp/catppuccinCursor/cursors/Catppuccin-Mocha-Dark-Cursors.zip -d $$HOME/.local/share/icons
