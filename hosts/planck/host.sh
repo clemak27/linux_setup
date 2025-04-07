440749858b867310d0f0a0036edf394271b40be8
clemak27 <clemak27@mailbox.org>
2025-04-07 19:18:29 +0200

    fix(planck): setup storage

--- hosts/planck/host.sh
+++ hosts/planck/host.sh
@@ -26,12 +26,16 @@ pkg install -y zsh starship zsh-completions
 "$host_dir/../../modules/zsh/module.sh"
 chsh -s zsh
 
-mkdir -p "$HOME/.termux"
-ln -sf "$host_dir/colors.properties" "$HOME/.termux/colors.properties"
-
 pkg install -y bat eza fd fzf htop jq yazi ripgrep sd tealdeer tree unrar unzip
 "$host_dir/../../modules/tools/module.sh"
+
+mkdir -p "$HOME/.termux"
+ln -sf "$host_dir/colors.properties" "$HOME/.termux/colors.properties"
 
 curl -L \
   --url https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/NoLigatures/Regular/JetBrainsMonoNLNerdFontMono-Regular.ttf \
   -o "$HOME/.termux/font.ttf"
+
+termux-setup-storage
+
+exit 0
