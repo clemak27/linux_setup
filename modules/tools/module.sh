3907eb28d1c8d3fc11879d8f6ba2b561899fa086
clemak27 <clemak27@mailbox.org>
2025-04-07 19:39:41 +0200

    fix(tools): set default yt-dlp outdir

--- modules/tools/module.sh
+++ modules/tools/module.sh
@@ -6,8 +6,9 @@ config_dir="$HOME/.config"
 bin_dir="$HOME/.local/bin"
 module_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-mkdir -p "$config_dir/bat"
+mkdir -p "$config_dir/bat" "$config_dir/yt-dlp"
 ln -sf "$module_dir/bat.config" "$config_dir/bat/config"
+echo '-o ~/Downloads/youtube/%(title)s.%(ext)s' > "$config_dir/yt-dlp/config"
 mkdir -p "$bin_dir"
 mkdir -p "$config_dir/tealdeer"
 ln -sf "$module_dir/tealdeer.toml" "$config_dir/tealdeer/config.toml"
