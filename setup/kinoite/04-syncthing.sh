2c03502444801dd07aaa852753ce95fe5ab80a2d
clemak27 <clemak27@mailbox.org>
2025-04-27 17:51:03 +0200

    fix(syncthing): add quadlet to chezmoi

--- setup/kinoite/04-syncthing.sh
+++ setup/kinoite/04-syncthing.sh
@@ -3,27 +3,6 @@
 set -xueo pipefail
 
 mkdir -p "$HOME/.local/state/syncthing"
-mkdir -p "$HOME/.config/containers/systemd"
-
-cat << EOF > "$HOME/.config/containers/systemd/syncthing.container"
-[Container]
-Image=docker.io/syncthing/syncthing
-AutoUpdate=registry
-Network=host
-Volume=%h:%h
-SecurityLabelDisable=true
-Environment=STHOMEDIR=%h/.local/state/syncthing
-Environment=HOME=%h
-UserNS=keep-id
-User=1000:1000
-
-[Service]
-Restart=always
-TimeoutStartSec=900
-
-[Install]
-WantedBy=default.target
-EOF
 
 systemctl --user daemon-reload
 systemctl --user start syncthing
