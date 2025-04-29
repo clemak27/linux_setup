305b18346eddf638ef7c608bc54b182ded128422
clemak27 <clemak27@mailbox.org>
2025-04-29 07:12:58 +0200

    fix(chezmoi): use proper tmp dir

--- dotfiles/run_onchange_customization.sh
+++ dotfiles/run_onchange_customization.sh
@@ -7,9 +7,10 @@ set -eo pipefail
 # renovate: datasource=github-tags depName=lassekongo83/adw-gtk3 versioning=loose
 adw_gtk3_version=6.0
 
-mkdir -p tmp "$HOME/.local/share/themes"
-curl -Lo tmp/adw-gtk3.tar.xz --url https://github.com/lassekongo83/adw-gtk3/releases/download/v${adw_gtk3_version}/adw-gtk3v${adw_gtk3_version}.tar.xz
-tar xf tmp/adw-gtk3.tar.xz --directory "$HOME/.local/share/themes"
+mkdir -p /tmp/adw-gtk "$HOME/.local/share/themes"
+curl -Lo /tmp/adw-gtk/adw-gtk3.tar.xz --url https://github.com/lassekongo83/adw-gtk3/releases/download/v${adw_gtk3_version}/adw-gtk3v${adw_gtk3_version}.tar.xz
+tar xf /tmp/adw-gtk/adw-gtk3.tar.xz --directory "$HOME/.local/share/themes"
+rm -rf /tmp/adw-gtk
 
 # papirus
 
