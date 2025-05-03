0323b4f8a22aa050ae78a480cef0760847b8cf89
clemak27 <clemak27@mailbox.org>
2025-05-03 09:34:16 +0200

    style(kinoite): formatting

--- setup/kinoite/setup.sh
+++ setup/kinoite/setup.sh
@@ -6,6 +6,8 @@ host_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 ## base
 
+sudo -v
+
 if ! command -v zsh &> /dev/null; then
   rpm-ostree install --idempotent --apply-live distrobox vim zsh
   sudo usermod -s /usr/bin/zsh clemens
@@ -18,7 +20,9 @@ sudo sed -i 's/enabled=1/enabled=0/' \
 
 if [ "$HOSTNAME" = "newton" ]; then
   # shit gpu
-  rpm-ostree install --idempotent https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-42.noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-42.noarch.rpm
+  rpm-ostree install \
+    "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+    "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
   rpm-ostree install --idempotent xorg-x11-drv-nvidia akmod-nvidia
   rpm-ostree kargs --append-if-missing=rd.driver.blacklist=nouveau --append-if-missing=modprobe.blacklist=nouveau --append-if-missing=nvidia-drm.modeset=1 initcall_blacklist=simpledrm_platform_driver_init
 fi
