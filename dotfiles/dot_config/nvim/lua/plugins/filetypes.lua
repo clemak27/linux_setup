f03567820c00baf5e81720b2f7c73bef180b10f6
clemak27 <clemak27@mailbox.org>
2025-05-24 16:25:15 +0200

    chore(nvim): move chezmoi.nvim config to new file

--- dotfiles/dot_config/nvim/lua/plugins/filetypes.lua
+++ dotfiles/dot_config/nvim/lua/plugins/filetypes.lua
@@ -49,46 +49,4 @@ return {
     "calops/hmts.nvim",
     version = "*",
   },
-  {
-    "xvzc/chezmoi.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("chezmoi").setup({
-        events = {
-          on_open = {
-            notification = {
-              enable = false,
-            },
-          },
-          on_watch = {
-            notification = {
-              enable = false,
-            },
-          },
-          on_apply = {
-            notification = {
-              enable = true,
-              msg = "Successfully applied",
-            },
-          },
-        },
-      })
-
-      vim.api.nvim_create_augroup("chezmoi_watch", { clear = true })
-      vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-        pattern = {
-          os.getenv("HOME") .. "/Projects/linux_setup/dotfiles/*",
-          "/var" .. os.getenv("HOME") .. "/Projects/linux_setup/dotfiles/*",
-        },
-        group = "chezmoi_watch",
-        callback = function(args)
-          local bufnr = args.buf
-          local edit_watch = function()
-            require("chezmoi.commands.__edit").watch(bufnr)
-          end
-          vim.schedule(edit_watch)
-        end,
-      })
-    end,
-  },
 }
