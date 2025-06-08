9b703bab8fac9800cbd87d65512dcf49b39939c7
clemak27 <clemak27@mailbox.org>
2025-06-08 14:43:49 +0200

    fix(chezmoi): add feishin to chezmoi

---
+++ dotfiles/.chezmoiscripts/run_onchange_feishin.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if [ "$HOSTNAME" = "fermi" ]; then
+  exit 0
+fi
+
+# renovate: datasource=github-tags depName=jeffvli/feishin versioning=loose
+feishin_version=0.14.0
+
+mkdir -p "$HOME/.local/bin" "$HOME/.local/share/applications"
+curl -L --url https://github.com/jeffvli/feishin/releases/download/v${feishin_version}/Feishin-${feishin_version}-linux-x86_64.AppImage -o "$HOME/.local/bin/feishin"
+chmod +x "$HOME/.local/bin/feishin"
+curl -L --url https://raw.githubusercontent.com/jeffvli/feishin/refs/heads/development/assets/icons/128x128.png -o "$HOME/.local/share/applications/feishin.png"
+echo -e "[Desktop Entry]\nName=Feishin\nExec=$HOME/.local/bin/feishin\nType=Application\nCategories=Multimedia\nIcon=$HOME/.local/share/applications/feishin.png" > "$HOME/.local/share/applications/feishin.desktop"
--- setup/kinoite/setup.sh
+++ setup/kinoite/setup.sh
@@ -82,15 +82,6 @@ flatpak install -y flathub \
   org.signal.Signal
 flatpak override --user --filesystem=xdg-config/gtk-3.0 --filesystem=xdg-config/gtk-4.0
 
-# renovate: datasource=github-tags depName=jeffvli/feishin versioning=loose
-feishin_version=0.14.0
-
-mkdir -p "$HOME/.local/bin" "$HOME/.local/share/applications"
-curl -L --url https://github.com/jeffvli/feishin/releases/download/v${feishin_version}/Feishin-${feishin_version}-linux-x86_64.AppImage -o "$HOME/.local/bin/feishin"
-chmod +x "$HOME/.local/bin/feishin"
-curl -L --url https://raw.githubusercontent.com/jeffvli/feishin/refs/heads/development/assets/icons/128x128.png -o "$HOME/.local/share/applications/feishin.png"
-echo -e "[Desktop Entry]\nName=Feishin\nExec=$HOME/.local/bin/feishin\nType=Application\nCategories=Multimedia\nIcon=$HOME/.local/share/applications/feishin.png" > "$HOME/.local/share/applications/feishin.desktop"
-
 ## wezterm
 
 flatpak install -y flathub org.wezfurlong.wezterm
