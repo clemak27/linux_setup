1fcc66a5e1700e27a10ec0a5343072f4879f4985
clemak27 <clemak27@mailbox.org>
2025-08-01 08:13:05 +0200

    fix(feishin): appimage does not contain version number anymore

--- dotfiles/.chezmoiscripts/run_onchange_feishin.sh
+++ dotfiles/.chezmoiscripts/run_onchange_feishin.sh
@@ -10,7 +10,7 @@ fi
 feishin_version=0.19.0
 
 mkdir -p "$HOME/.local/bin" "$HOME/.local/share/applications"
-curl -L --url https://github.com/jeffvli/feishin/releases/download/v${feishin_version}/Feishin-${feishin_version}-linux-x86_64.AppImage -o "$HOME/.local/bin/feishin"
+curl -L --url https://github.com/jeffvli/feishin/releases/download/v${feishin_version}/Feishin-linux-x86_64.AppImage -o "$HOME/.local/bin/feishin"
 chmod +x "$HOME/.local/bin/feishin"
 curl -L --url https://raw.githubusercontent.com/jeffvli/feishin/refs/heads/development/assets/icons/128x128.png -o "$HOME/.local/share/applications/feishin.png"
 echo -e "[Desktop Entry]\nName=Feishin\nExec=$HOME/.local/bin/feishin\nType=Application\nCategories=Multimedia\nIcon=$HOME/.local/share/applications/feishin.png" > "$HOME/.local/share/applications/feishin.desktop"
