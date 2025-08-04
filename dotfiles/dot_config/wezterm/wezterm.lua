f4e5b9afa82846b9232f968ec5cc20d4959e975f
clemak27 <clemak27@mailbox.org>
2025-08-04 09:12:46 +0200

    fix(plasma): improve styling

--- dotfiles/dot_config/wezterm/wezterm.lua
+++ dotfiles/dot_config/wezterm/wezterm.lua
@@ -62,10 +62,10 @@ local weztermCfg = {
   window_decorations = "TITLE | RESIZE",
   window_close_confirmation = "NeverPrompt",
   window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
+    left = 1,
+    right = 1,
+    top = 1,
+    bottom = 1,
   },
 
   audible_bell = "Disabled",
