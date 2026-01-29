d087f2a65dcaf6866c3b7bdd684207e5c953f9e3
clemak27 <clemak27@mailbox.org>
2026-01-29 16:32:30 +0100

    chore(setup): cleanup

--- setup/kinoite/setup.sh
+++ setup/kinoite/setup.sh
@@ -16,24 +16,9 @@ if [ "$HOSTNAME" = "newton" ]; then
     --append-if-missing=initcall_blacklist=simpledrm_platform_driver_init
 fi
 
-## podman
-
-systemctl --user enable podman.socket
-
 ## brew
 
-brew_version="5.0.0"
 brew_dir="/home/linuxbrew/.linuxbrew"
-
-curl -fL -o /tmp/homebrew.tar.gz https://github.com/Homebrew/brew/archive/refs/tags/$brew_version.tar.gz
-mkdir -p /tmp/homebrew
-tar -xvf /tmp/homebrew.tar.gz -C /tmp/homebrew
-sudo mkdir -p "$brew_dir/bin" "$brew_dir/share/zsh/site-functions" "$brew_dir/Cellar"
-sudo chown -R 1000:1000 $brew_dir
-cp -R -n /tmp/homebrew/brew-$brew_version "$brew_dir/Homebrew"
-ln -sf "$brew_dir/Homebrew/bin/brew" "$brew_dir/bin/brew"
-ln -sf "$brew_dir/Homebrew/share/zsh/site-functions/_brew" "$brew_dir/share/zsh/site-functions/_brew"
-rm -rf /tmp/homebrew /tmp/homebrew.tar.gz
 
 export HOMEBREW_CELLAR=$brew_dir/Cellar
 export HOMEBREW_PREFIX=$brew_dir
