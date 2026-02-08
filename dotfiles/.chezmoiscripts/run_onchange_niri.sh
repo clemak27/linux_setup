e64fff57bb4ec7a16d6abd91237cd208840e5e62
clemak27 <clemak27@mailbox.org>
2026-02-08 22:04:02 +0100

    fix(dms): set some config

--- dotfiles/.chezmoiscripts/run_onchange_niri.sh
+++ dotfiles/.chezmoiscripts/run_onchange_niri.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-config_dir="$HOME/.config"
+dms_config="$HOME/.config/DankMaterialShell/settings.json"
 
 if [ "$XDG_CURRENT_DESKTOP" != "niri" ]; then
   exit 0
@@ -14,3 +14,18 @@ gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
 gsettings set org.gnome.nautilus.preferences default-sort-order 'name'
 gsettings set org.gnome.nautilus.preferences show-hidden-files true
 gsettings set org.gtk.Settings.FileChooser sort-directories-first true
+
+if [ -f "$dms_config" ]; then
+  yq -iP '.widgetBackgroundColor = "s"' "$dms_config" -o json
+  yq -iP '.niriLayoutGapsOverride = 10' "$dms_config" -o json
+  yq -iP '.cursorSettings.theme = "breeze_cursors"' "$dms_config" -o json
+  yq -iP '.iconTheme = "Papirus-Dark"' "$dms_config" -o json
+  yq -iP '.fontFamily = "Noto Sans"' "$dms_config" -o json
+  yq -iP '.monoFontFamily = "JetBrainsMonoNL Nerd Font"' "$dms_config" -o json
+  yq -iP '.clockDateFormat = "dd.MM.yyyy"' "$dms_config" -o json
+  yq -iP '.useAutoLocation = true' "$dms_config" -o json
+  yq -iP '.centeringMode = "geometric"' "$dms_config" -o json
+  yq -iP '.osdPosition = 7' "$dms_config" -o json
+  yq -iP '.osdAlwaysShowValue = true' "$dms_config" -o json
+  yq -iP '.lockBeforeSuspend = true' "$dms_config" -o json
+fi
