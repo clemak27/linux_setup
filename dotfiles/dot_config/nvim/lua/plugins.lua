1caa4ce740091e961657ff5cd1e2a65c187a40e3
clemak27 <clemak27@mailbox.org>
2026-02-16 21:35:57 +0100

    feat(nvim): add dial.nvim

--- dotfiles/dot_config/nvim/lua/plugins.lua
+++ dotfiles/dot_config/nvim/lua/plugins.lua
@@ -70,4 +70,46 @@ return {
       })
     end,
   },
+  {
+    "monaqa/dial.nvim",
+    config = function()
+      local augend = require("dial.augend")
+      local map = require("dial.map")
+
+      require("dial.config").augends:register_group({
+        default = {
+          augend.integer.alias.decimal,
+          augend.date.alias["%Y-%m-%d"],
+          augend.date.alias["%d.%m.%Y"],
+          augend.date.alias["%d.%m."],
+          augend.semver.alias.semver,
+        },
+      })
+
+      vim.keymap.set("n", "<C-a>", function()
+        map.manipulate("increment", "normal")
+      end)
+      vim.keymap.set("n", "<C-x>", function()
+        map.manipulate("decrement", "normal")
+      end)
+      vim.keymap.set("n", "g<C-a>", function()
+        map.manipulate("increment", "gnormal")
+      end)
+      vim.keymap.set("n", "g<C-x>", function()
+        map.manipulate("decrement", "gnormal")
+      end)
+      vim.keymap.set("x", "<C-a>", function()
+        map.manipulate("increment", "visual")
+      end)
+      vim.keymap.set("x", "<C-x>", function()
+        map.manipulate("decrement", "visual")
+      end)
+      vim.keymap.set("x", "g<C-a>", function()
+        map.manipulate("increment", "gvisual")
+      end)
+      vim.keymap.set("x", "g<C-x>", function()
+        map.manipulate("decrement", "gvisual")
+      end)
+    end,
+  },
 }
