f9697441ce4912dc7516891e82ca6632c9cfbf3a
clemak27 <clemak27@mailbox.org>
2026-04-01 07:06:16 +0200

    fix(nvim): use builtin nohlsearch plugin

--- dotfiles/dot_config/nvim/init.lua
+++ dotfiles/dot_config/nvim/init.lua
@@ -118,19 +118,7 @@ vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "WinLeave"
 })
 
 -- don't show highlights after searching
-local ns = vim.api.nvim_create_namespace("toggle_hlsearch")
-local function toggle_hlsearch(char)
-  if vim.fn.mode() == "n" then
-    local keys = { "<CR>", "n", "N", "*", "#", "?", "/" }
-    local new_hlsearch = vim.tbl_contains(keys, vim.fn.keytrans(char))
-
-    if vim.opt.hlsearch:get() ~= new_hlsearch then
-      vim.opt.hlsearch = new_hlsearch
-    end
-  end
-end
-
-vim.on_key(toggle_hlsearch, ns)
+vim.cmd("packadd nohlsearch")
 
 ----------------------------------------- mappings -----------------------------------------
 
